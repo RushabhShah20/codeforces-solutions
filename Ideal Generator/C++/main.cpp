fd45787f7c8bb18eb81d7d178ffe6f493f97b00e
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 20:36:24 +0530

    Added C++ solution to 'Ideal Generator'

---
+++ Ideal Generator/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Ideal Generator
+// Link to the problem: https://codeforces.com/contest/2093/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n % 2 == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
