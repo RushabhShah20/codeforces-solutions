780556b7e8839115c18e9c42e1958396a99397fa
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 11:32:30 +0530

    Added C++ solution to 'Cloudberry Jam'

---
+++ Cloudberry Jam/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Cloudberry Jam
+// Link to the problem: https://codeforces.com/contest/2086/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << 2 * n << endl;
+    }
+    return 0;
+}
