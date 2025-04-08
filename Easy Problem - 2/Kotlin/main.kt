aae2d754b92771adb33168489ad8f6503a3623f8
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 11:33:41 +0530

    Added Kotlin solution to 'Easy Problem - 2'

---
+++ Easy Problem - 2/Kotlin/main.kt
@@ -0,0 +1,12 @@
+// Problem: Easy Problem
+// Link to the problem: https://codeforces.com/contest/2088/problem/A
+fun main() {
+    val input = readLine()
+    var a = input!!.toInt()
+    while(a>0){
+        val n = readLine()
+        var b = n!!.toInt()
+        println(b-1);
+        a--;
+    }
+}
