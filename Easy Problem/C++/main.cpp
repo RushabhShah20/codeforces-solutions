fccda24db89a1e9f292d63d1d48bec2c8d4cb607
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 11:33:21 +0530

    Added C++ solution to 'Easy Problem'

---
+++ Easy Problem/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Easy Problem
+// Link to the problem: https://codeforces.com/contest/2044/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n - 1 << endl;
+    }
+    return 0;
+}
