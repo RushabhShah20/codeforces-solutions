d2f2b9c5a4c871867539b5043670e7b4197c4e32
Rushabh <20rush09abh2001@gmail.com>
2025-06-04 13:36:56 +0530

    Added C++ solution to 'Domino piling'

---
+++ Domino piling/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Domino piling
+// Link to the problem: https://codeforces.com/contest/50/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll m, n;
+    cin >> m >> n;
+    cout << ((m * n) / 2) << endl;
+    return 0;
+}
