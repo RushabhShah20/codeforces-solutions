8c87c68935c2f9ac88ec3d4e32dd48f8180075bd
Rushabh <20rush09abh2001@gmail.com>
2025-10-31 15:54:56 +0530

    Added C++ solution to 'Pizza Time'

---
+++ Solutions/Pizza Time/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Pizza Time
+// Link to the problem: https://codeforces.com/contest/2156/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (n - 1) / 2 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
