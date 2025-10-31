447bfaaf185a2184b4e72409238fbaa6969798f1
Rushabh <20rush09abh2001@gmail.com>
2025-10-31 15:59:00 +0530

    Added C++ solution to 'Square?'

---
+++ Solutions/Square/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Square?
+// Link to the problem: https://codeforces.com/contest/2167/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == c && b == d && a == b)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
