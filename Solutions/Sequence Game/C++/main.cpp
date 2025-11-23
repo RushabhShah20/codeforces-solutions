f9c11c3b3b5230bcc81124b219dfcd52d491af8c
Rushabh <20rush09abh2001@gmail.com>
2025-11-23 14:43:21 +0530

    Added C++ solution to 'Sequence Game'

---
+++ Solutions/Sequence Game/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Sequence Game
+// Link to the problem: https://codeforces.com/contest/2164/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll x;
+    cin >> x;
+    ll c = *min_element(a.begin(), a.end()), d = *max_element(a.begin(), a.end());
+    if (x >= c && x <= d)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
