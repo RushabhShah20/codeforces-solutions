b5d50304cc9ee3591456168929dcfaa3ead24289
Rushabh <20rush09abh2001@gmail.com>
2026-01-12 16:45:02 +0530

    Added C++ solution to 'Coins'

---
+++ Solutions/Coins/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Coins
+// Link to the problem: https://codeforces.com/contest/1814/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n % 2 == 0 || (n - k) % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
