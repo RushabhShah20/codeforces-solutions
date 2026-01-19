d4705ba5824d497faa2c3378ed570db3a7865a73
Rushabh <20rush09abh2001@gmail.com>
2026-01-19 15:29:25 +0530

    Added C++ solution to 'Perfect Root'

---
+++ Solutions/Perfect Root/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Perfect Root
+// Link to the problem: https://codeforces.com/contest/2185/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+    {
+        cout << i * i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
