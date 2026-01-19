c720c0d64f717241f508d6e92b39f30a934dfb92
Rushabh <20rush09abh2001@gmail.com>
2026-01-19 13:32:52 +0530

    Added C++ solution to 'Everybody Likes Good Arrays!'

---
+++ Solutions/Everybody Likes Good Arrays!/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Everybody Likes Good Arrays!
+// Link to the problem: https://codeforces.com/contest/1777/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] % 2 != a[i - 1] % 2)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
