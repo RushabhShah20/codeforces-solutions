cc7abdf0bf55bfee9c580ed912291dd3b99d0378
Rushabh <20rush09abh2001@gmail.com>
2026-01-20 10:54:30 +0530

    Added C++ solution to 'Odd Divisor'

---
+++ Solutions/Odd Divisor/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Odd Divisor
+// Link to the problem: https://codeforces.com/contest/1475/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    while (n % 2 == 0)
+    {
+        n /= 2;
+    }
+    if (n == 1)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
