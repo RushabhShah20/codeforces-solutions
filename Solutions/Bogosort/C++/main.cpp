2d0cda16b42c67566ce4041545000e05d4c6d563
Rushabh <20rush09abh2001@gmail.com>
2026-01-20 13:31:37 +0530

    Added C++ solution to 'Bogosort'

---
+++ Solutions/Bogosort/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Bogosort
+// Link to the problem: https://codeforces.com/contest/1312/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<ll>());
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
