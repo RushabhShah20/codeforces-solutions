ace2fad056a9c04c8fc3bfedf41c013ca206b49e
Rushabh <20rush09abh2001@gmail.com>
2026-01-23 19:44:55 +0530

    Added C++ solution to 'Goals of Victory'

---
+++ Solutions/Goals of Victory/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Goals of Victory
+// Link to the problem: https://codeforces.com/contest/1877/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = 0; i < n - 1; i++)
+    {
+        ll x;
+        cin >> x;
+        ans -= x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
