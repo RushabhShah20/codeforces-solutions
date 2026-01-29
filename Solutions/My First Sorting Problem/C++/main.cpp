c618f5a0b68e38880ca30f76158706083f345726
Rushabh <20rush09abh2001@gmail.com>
2026-01-29 13:41:46 +0530

    Added C++ solution to 'My First Sorting Problem'

---
+++ Solutions/My First Sorting Problem/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: My First Sorting Problem
+// Link to the problem: https://codeforces.com/contest/1971/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << min(x, y) << " " << max(x, y) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
