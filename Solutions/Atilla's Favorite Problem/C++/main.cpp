c92200e4378c8b05f399a0f25b7e13f5fe390bc9
Rushabh <20rush09abh2001@gmail.com>
2026-01-30 15:21:13 +0530

    Added C++ solution to 'Atilla's Favorite Problem'

---
+++ Solutions/Atilla's Favorite Problem/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Atilla's Favorite Problem
+// Link to the problem: https://codeforces.com/contest/1760/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll ans = 1;
+    for (const char &c : s)
+    {
+        ans = max(ans, (ll)(c - 'a' + 1));
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
