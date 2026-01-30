c93bffe9ddcb63082a68c97ef2ac99c98f16f779
Rushabh <20rush09abh2001@gmail.com>
2026-01-30 15:24:00 +0530

    Added C++ solution to 'Sum'

---
+++ Solutions/Sum/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Sum
+// Link to the problem: https://codeforces.com/contest/1742/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c || b + c == a || a + c == b)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
