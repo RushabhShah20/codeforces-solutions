2e94bb17fb1173ea08251af7353f9a4c485dce11
Rushabh <20rush09abh2001@gmail.com>
2026-02-01 14:42:46 +0530

    Added C++ solution to 'Triple'

---
+++ Solutions/Triple/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Triple
+// Link to the problem: https://codeforces.com/contest/1669/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (ll i = 0; i < n - 2; i++)
+    {
+        if (a[i] == a[i + 2])
+        {
+            cout << a[i] << endl;
+            return;
+        }
+    }
+    cout << -1 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
