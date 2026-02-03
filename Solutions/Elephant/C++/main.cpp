8f33a2582096605d181846389d57c9433f090556
Rushabh <20rush09abh2001@gmail.com>
2026-02-03 13:52:52 +0530

    Added C++ solution to 'Elephant'

---
+++ Solutions/Elephant/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Elephant
+// Link to the problem: https://codeforces.com/contest/617/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    const ll ans = (x + 4) / 5;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
