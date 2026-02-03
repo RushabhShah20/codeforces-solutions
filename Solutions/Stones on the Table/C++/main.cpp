3082da72f6a2a8c38d08a4f06ec21a6564968bc9
Rushabh <20rush09abh2001@gmail.com>
2026-02-03 13:56:31 +0530

    Added C++ solution to 'Stones on the Table'

---
+++ Solutions/Stones on the Table/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Stones on the Table
+// Link to the problem: https://codeforces.com/contest/266/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll ans = 0;
+    for (ll i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
