0cdfae4a6ad7e8d47329c6e2c4276386d6797366
Rushabh <20rush09abh2001@gmail.com>
2026-02-04 15:04:52 +0530

    Added C++ solution to 'Make It Zero'

---
+++ Solutions/Make It Zero/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Make It Zero
+// Link to the problem: https://codeforces.com/contest/1869/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+    }
+    if (n % 2 == 0)
+    {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+    else
+    {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
