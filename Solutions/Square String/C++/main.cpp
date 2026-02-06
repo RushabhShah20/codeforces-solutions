31dac3abd3e82c03d804478e20d57deeda87332d
Rushabh <20rush09abh2001@gmail.com>
2026-02-06 15:23:37 +0530

    Added C++ solution to 'Square String?'

---
+++ Solutions/Square String/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Square String?
+// Link to the problem: https://codeforces.com/contest/1619/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size();
+    if (n % 2 == 0)
+    {
+        const string a = s.substr(0, n / 2), b = s.substr(n / 2, n / 2);
+        if (a == b)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
