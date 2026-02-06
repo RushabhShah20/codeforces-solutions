4b6f0bb726b7b4824667a2f2f00451fd8ffce63c
Rushabh <20rush09abh2001@gmail.com>
2026-02-06 15:30:02 +0530

    Added C++ solution to 'Good Kid'

---
+++ Solutions/Good Kid/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Good Kid
+// Link to the problem: https://codeforces.com/contest/1873/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = a[0] + 1;
+    for (ll i = 1; i < n; i++)
+    {
+        ans *= a[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
