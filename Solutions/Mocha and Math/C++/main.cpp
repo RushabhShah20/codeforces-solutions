253cc24dcff20f48bae04e7832d7262458493f68
Rushabh <20rush09abh2001@gmail.com>
2026-02-06 11:52:09 +0530

    Added C++ solution to 'Mocha and Math'

---
+++ Solutions/Mocha and Math/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Mocha and Math
+// Link to the problem: https://codeforces.com/contest/1559/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = a[0];
+    for (ll i = 1; i < n; i++)
+    {
+        ans &= a[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
