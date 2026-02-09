afef637feb1acb907473c662b22e475a0ecc4964
Rushabh <20rush09abh2001@gmail.com>
2026-02-09 15:21:01 +0530

    Added C++ solution to 'String Task'

---
+++ Solutions/String Task/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: String Task
+// Link to the problem: https://codeforces.com/contest/118/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    string ans;
+    for (const char &c : s)
+    {
+        if (!(tolower(c) == 'a' || tolower(c) == 'e' || tolower(c) == 'i' || tolower(c) == 'o' || tolower(c) == 'u' || tolower(c) == 'y'))
+        {
+            ans += '.';
+            ans += tolower(c);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
