b38120cb46e11a4e57edfb6ec15167356cda961a
Rushabh <20rush09abh2001@gmail.com>
2026-02-09 15:32:10 +0530

    Added C++ solution to 'Gravity Flip'

---
+++ Solutions/Gravity Flip/C++/main.cpp
@@ -0,0 +1,34 @@
+// Problem: Gravity Flip
+// Link to the problem: https://codeforces.com/contest/405/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
