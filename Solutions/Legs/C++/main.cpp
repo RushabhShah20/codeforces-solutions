7752f7fd654403acebc7b11ae4fcf92b51e65031
Rushabh <20rush09abh2001@gmail.com>
2026-02-13 16:49:24 +0530

    Added C++ solution to 'Legs'

---
+++ Solutions/Legs/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Legs
+// Link to the problem: https://codeforces.com/contest/1996/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 4 == 0)
+    {
+        cout << (n / 4) << endl;
+    }
+    else
+    {
+        cout << ((n / 4) + 1) << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
