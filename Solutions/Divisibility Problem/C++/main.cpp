385fedfc591959d716975d216ef334baadd6681f
Rushabh <20rush09abh2001@gmail.com>
2026-02-15 11:43:14 +0530

    Added C++ solution to 'Divisibility Problem'

---
+++ Solutions/Divisibility Problem/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Divisibility Problem
+// Link to the problem: https://codeforces.com/contest/1328/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll ans = ((a % b == 0) ? (0) : (b - (a % b)));
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
