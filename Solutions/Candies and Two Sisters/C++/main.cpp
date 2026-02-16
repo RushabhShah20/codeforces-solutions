71ba8f0c1dbfcc1c3dfd89dfeee05dc11d52fc56
Rushabh <20rush09abh2001@gmail.com>
2026-02-16 15:11:27 +0530

    Added C++ solution to 'Candies and Two Sisters'

---
+++ Solutions/Candies and Two Sisters/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Candies and Two Sisters
+// Link to the problem: https://codeforces.com/contest/1335/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n % 2 == 0) ? ((n - 1) / 2) : (n / 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
