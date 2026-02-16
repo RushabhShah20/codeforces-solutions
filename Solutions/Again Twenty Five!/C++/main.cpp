df88c3d3140e476f8bedf184bdb9c823544f57df
Rushabh <20rush09abh2001@gmail.com>
2026-02-16 17:15:04 +0530

    Added C++ solution to 'Again Twenty Five!'

---
+++ Solutions/Again Twenty Five!/C++/main.cpp
@@ -0,0 +1,24 @@
+// Problem: Again Twenty Five!
+// Link to the problem: https://codeforces.com/contest/630/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << 25 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
