0372e4073e8d502e3c38aa78dc4837d6865017ae
Rushabh <20rush09abh2001@gmail.com>
2026-02-17 16:44:13 +0530

    Added C++ solution to 'To My Critics'

---
+++ Solutions/To My Critics/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: To My Critics
+// Link to the problem: https://codeforces.com/contest/1850/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if (a + b >= 10 || b + c >= 10 || a + c >= 10)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
