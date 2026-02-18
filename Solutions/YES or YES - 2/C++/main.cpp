9ab8174380c370a3fc2bac37b0e5d49d097c851a
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 15:01:42 +0530

    Added C++ solution to 'YES or YES?'

---
+++ Solutions/YES or YES - 2/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: YES or YES?
+// Link to the problem: https://codeforces.com/contest/1703/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (tolower(s[0]) == 'y' && tolower(s[1]) == 'e' && tolower(s[2]) == 's')
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
