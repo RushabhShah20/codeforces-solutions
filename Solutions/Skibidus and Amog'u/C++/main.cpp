5403ee5929a218afa155d822b839e3c9c668153d
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 14:12:49 +0530

    Added C++ solution to 'Skibidus and Amog'u'

---
+++ Solutions/Skibidus and Amog'u/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Skibidus and Amog'u
+// Link to the problem: https://codeforces.com/contest/2065/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size();
+    s.erase(n - 2, 2);
+    s += "i";
+    cout << s << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
