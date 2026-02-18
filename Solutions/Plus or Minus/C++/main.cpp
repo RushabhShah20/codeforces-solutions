84ce5bb190dc14fcc0e2006f34e829b80ee1139a
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 14:33:15 +0530

    Added C++ solution to 'Plus or Minus'

---
+++ Solutions/Plus or Minus/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Plus or Minus
+// Link to the problem: https://codeforces.com/contest/1807/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c)
+    {
+        cout << "+" << endl;
+    }
+    else
+    {
+        cout << "-" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
