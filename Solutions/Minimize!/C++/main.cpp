51abeb21a9a554391973a194b7eb396472258034
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 14:18:45 +0530

    Added C++ solution to 'Minimize!'

---
+++ Solutions/Minimize!/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Minimize!
+// Link to the problem: https://codeforces.com/contest/2009/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll ans = b - a;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
