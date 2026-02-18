9ecb0cf8657f1a4f5cb091c1ece5c175cd669f07
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 14:25:45 +0530

    Added C++ solution to 'A+B Again?'

---
+++ Solutions/A+B Again/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: A+B Again?
+// Link to the problem: https://codeforces.com/contest/1999/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll ans = ((s.front() - '0') + (s.back() - '0'));
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
