511a1fe84b8f2f95bceed6f62a391e98e7c55f72
Rushabh <20rush09abh2001@gmail.com>
2026-02-18 17:10:49 +0530

    Added C++ solution to 'Aleksa and Stack'

---
+++ Solutions/Aleksa and Stack/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Aleksa and Stack
+// Link to the problem: https://codeforces.com/contest/1878/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << 2 * i + 1 << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
