fa96a687cc0a0b1137035806e0b91e7b6771544e
Rushabh <20rush09abh2001@gmail.com>
2026-02-19 15:10:23 +0530

    Added C++ solution to 'Maximum Multiple Sum'

---
+++ Solutions/Maximum Multiple Sum/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Maximum Multiple Sum
+// Link to the problem: https://codeforces.com/contest/1985/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n == 3 ? n : 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
