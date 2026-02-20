07b7d65086875febdd01a5bff66d195e32340844
Rushabh <20rush09abh2001@gmail.com>
2026-02-20 15:50:22 +0530

    Added C++ solution to 'Yet Another Two Integers Problem'

---
+++ Solutions/Yet Another Two Integers Problem/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Yet Another Two Integers Problem
+// Link to the problem: https://codeforces.com/contest/1409/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll ans = (abs(a - b) + 9) / 10;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
