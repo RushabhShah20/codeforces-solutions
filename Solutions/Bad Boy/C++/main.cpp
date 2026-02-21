c43be6a2d908efe75c33966a4283d43961fb99dc
Rushabh <20rush09abh2001@gmail.com>
2026-02-21 13:50:32 +0530

    Added C++ solution to 'Bad Boy'

---
+++ Solutions/Bad Boy/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Bad Boy
+// Link to the problem: https://codeforces.com/contest/1537/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m, i, j;
+    cin >> n >> m >> i >> j;
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
