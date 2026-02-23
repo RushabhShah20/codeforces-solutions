49f478b53a3cccb47e9d37475db528fdb0fe7c1a
Rushabh <20rush09abh2001@gmail.com>
2026-02-23 13:50:35 +0530

    Added C++ solution to 'HQ9+'

---
+++ Solutions/HQ9+/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: HQ9+
+// Link to the problem: https://codeforces.com/contest/133/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.find('H') != string::npos || s.find('Q') != string::npos || s.find('9') != string::npos)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
