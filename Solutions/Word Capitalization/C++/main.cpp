5c3bca75da28ee3eb666f1b284c58c31534f3f73
Rushabh <20rush09abh2001@gmail.com>
2026-02-24 13:58:53 +0530

    Added C++ solution to 'Word Capitalization'

---
+++ Solutions/Word Capitalization/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Word Capitalization
+// Link to the problem: https://codeforces.com/contest/281/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
