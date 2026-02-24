f1470365ed1bcbaa46be01cac04c11da85c44efa
Rushabh <20rush09abh2001@gmail.com>
2026-02-24 14:02:43 +0530

    Added C++ solution to 'Bear and Big Brother'

---
+++ Solutions/Bear and Big Brother/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Bear and Big Brother
+// Link to the problem: https://codeforces.com/contest/791/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    ll ans = 0;
+    while (a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        ans++;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
