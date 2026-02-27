840b68ec5ef721e0ad8c3b67d17498b1881abc7a
Rushabh <20rush09abh2001@gmail.com>
2026-02-27 16:58:50 +0530

    Added C++ solution to 'Different String'

---
+++ Solutions/Different String/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Different String
+// Link to the problem: https://codeforces.com/contest/1971/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size();
+    const string t = s.substr(1, n - 1) + s.front();
+    if (s == t)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+        cout << t << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
