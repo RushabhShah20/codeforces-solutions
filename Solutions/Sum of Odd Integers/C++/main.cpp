9c97923c6d1da2e051a4aa83a84c3770bf09f78f
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 16:11:32 +0530

    Added C++ solution to 'Sum of Odd Integers'

---
+++ Solutions/Sum of Odd Integers/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Sum of Odd Integers
+// Link to the problem: https://codeforces.com/contest/1327/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n >= k * k && (n % 2 == k % 2))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
