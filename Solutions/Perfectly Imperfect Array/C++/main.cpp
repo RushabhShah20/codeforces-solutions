d503abda8891ec5a267394a2090b92bb500a1258
Rushabh <20rush09abh2001@gmail.com>
2026-03-08 11:31:08 +0530

    Added C++ solution to 'Perfectly Imperfect Array'

---
+++ Solutions/Perfectly Imperfect Array/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Perfectly Imperfect Array
+// Link to the problem: https://codeforces.com/contest/1514/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        const ll x = (ll)sqrtl(a[i]);
+        if (x * x != a[i])
+        {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
