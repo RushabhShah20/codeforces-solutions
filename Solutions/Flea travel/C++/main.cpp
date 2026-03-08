e1267adf49074349d42aefa657360830235b77d7
Rushabh <20rush09abh2001@gmail.com>
2026-03-08 16:59:37 +0530

    Added C++ solution to 'Flea travel'

---
+++ Solutions/Flea travel/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Flea travel
+// Link to the problem: https://codeforces.com/contest/55/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    while (n % 2 == 0)
+    {
+        n /= 2;
+    }
+    if (n == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
