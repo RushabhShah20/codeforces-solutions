6bbc27d3075d4039fe77d51285060988cea3af3a
Rushabh <20rush09abh2001@gmail.com>
2026-03-08 16:48:51 +0530

    Added C++ solution to 'Triangular numbers'

---
+++ Solutions/Triangular numbers/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Triangular numbers
+// Link to the problem: https://codeforces.com/contest/47/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll m = (ll)sqrtl(8 * n + 1);
+    if (m * m == 8 * n + 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
