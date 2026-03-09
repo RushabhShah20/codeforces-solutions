0ae15e123837a9fddea794cc47355821e607d6c9
Rushabh <20rush09abh2001@gmail.com>
2026-03-09 13:52:54 +0530

    Added C++ solution to 'Boy or Girl'

---
+++ Solutions/Boy or Girl/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Boy or Girl
+// Link to the problem: https://codeforces.com/contest/236/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    unordered_set<char> t;
+    for (const char c : s)
+    {
+        t.insert(c);
+    }
+    if (t.size() % 2 == 0)
+    {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else
+    {
+        cout << "IGNORE HIM!" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
