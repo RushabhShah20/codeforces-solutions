34697df4f5560778bb49a7d6d363abc7873cf61d
Rushabh <20rush09abh2001@gmail.com>
2026-03-10 16:59:13 +0530

    Added C++ solution to 'Maximum Increase'

---
+++ Solutions/Maximum Increase/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Maximum Increase
+// Link to the problem: https://codeforces.com/contest/702/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0, x = 1;
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] > a[i - 1])
+        {
+            x++;
+        }
+        else
+        {
+            ans = max(ans, x);
+            x = 1;
+        }
+    }
+    ans = max(ans, x);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
