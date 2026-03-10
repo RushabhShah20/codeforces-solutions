c5265af91c57c4a022cf3bd47e8e9d97809a4c14
Rushabh <20rush09abh2001@gmail.com>
2026-03-10 18:13:27 +0530

    Added C++ solution to 'Honest Coach'

---
+++ Solutions/Honest Coach/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Honest Coach
+// Link to the problem: https://codeforces.com/contest/1360/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = LLONG_MAX;
+    for (ll i = 1; i < n; i++)
+    {
+        ans = min(ans, a[i] - a[i - 1]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
