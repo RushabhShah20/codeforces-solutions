4a5a2154db5722c240312b83791ab6793ecae1b1
Rushabh <20rush09abh2001@gmail.com>
2026-03-10 14:15:55 +0530

    Added C++ solution to 'Magnets'

---
+++ Solutions/Magnets/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Magnets
+// Link to the problem: https://codeforces.com/contest/344/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, ans = 1;
+    cin >> n;
+    vector<string> s(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> s[i];
+    }
+    for (ll i = 1; i < n; i++)
+    {
+        if (s[i] != s[i - 1])
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
