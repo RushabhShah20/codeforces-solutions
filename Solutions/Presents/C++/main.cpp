22af6ff48f9e3dd580a4559d772820b730651d60
Rushabh <20rush09abh2001@gmail.com>
2026-03-10 15:38:38 +0530

    Added C++ solution to 'Presents'

---
+++ Solutions/Presents/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Presents
+// Link to the problem: https://codeforces.com/contest/136/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x] = i + 1;
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << m[i + 1] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
