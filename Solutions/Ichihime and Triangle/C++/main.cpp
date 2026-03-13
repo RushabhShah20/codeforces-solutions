cc7e6b82e0213f9b57145f5efb71260f774ba50a
Rushabh <20rush09abh2001@gmail.com>
2026-03-13 17:36:41 +0530

    Added C++ solution to 'Ichihime and Triangle'

---
+++ Solutions/Ichihime and Triangle/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Ichihime and Triangle
+// Link to the problem: https://codeforces.com/contest/1337/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
