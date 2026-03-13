e3336a6efcba18c880d30d41a695d6c9027162a8
Rushabh <20rush09abh2001@gmail.com>
2026-03-13 15:28:54 +0530

    Added C++ solution to 'Array Stabilization'

---
+++ Solutions/Array Stabilization/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Array Stabilization
+// Link to the problem: https://codeforces.com/contest/1095/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll ans = min(a[n - 1] - a[1], a[n - 2] - a[0]);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
