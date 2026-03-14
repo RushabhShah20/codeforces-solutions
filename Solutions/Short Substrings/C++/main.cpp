8103bdce6d1fccc8f5f28b0dfb56b2cf58592bfe
Rushabh <20rush09abh2001@gmail.com>
2026-03-14 15:31:25 +0530

    Added C++ solution to 'Short Substrings'

---
+++ Solutions/Short Substrings/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Short Substrings
+// Link to the problem: https://codeforces.com/contest/1367/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size();
+    string ans = s.substr(0, 2);
+    for (ll i = 3; i < n; i += 2)
+    {
+        ans += s[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
