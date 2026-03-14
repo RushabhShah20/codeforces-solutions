d8d5bb4c4a4755097f58e060adbbb03671522ebe
Rushabh <20rush09abh2001@gmail.com>
2026-03-14 15:38:44 +0530

    Added C++ solution to 'Boring Apartments'

---
+++ Solutions/Boring Apartments/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Boring Apartments
+// Link to the problem: https://codeforces.com/contest/1433/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size(), ans = 10 * (s.back() - '0' - 1) + n * (n + 1) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
