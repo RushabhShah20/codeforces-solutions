b1ce4404eb135d3afd7950ebd60e3f670c28a45b
Rushabh <20rush09abh2001@gmail.com>
2026-03-15 11:32:57 +0530

    Added C++ solution to 'Bad Triangle'

---
+++ Solutions/Bad Triangle/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Bad Triangle
+// Link to the problem: https://codeforces.com/contest/1398/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (a[0] + a[1] <= a[n - 1])
+    {
+        cout << 1 << " " << 2 << " " << n << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
