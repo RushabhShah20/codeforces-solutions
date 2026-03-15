575c9b0210ea5b1af82e6f693bd53ce6a98af1d9
Rushabh <20rush09abh2001@gmail.com>
2026-03-15 11:35:58 +0530

    Added C++ solution to 'Two Regular Polygons'

---
+++ Solutions/Two Regular Polygons/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Two Regular Polygons
+// Link to the problem: https://codeforces.com/contest/1312/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if (__gcd(m, n) == m)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
