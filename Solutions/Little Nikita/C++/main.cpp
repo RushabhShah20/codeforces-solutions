ad030257d74500b530c7e542c6b2dbb6271dbdb9
Rushabh <20rush09abh2001@gmail.com>
2026-03-16 13:35:42 +0530

    Added C++ solution to 'Little Nikita'

---
+++ Solutions/Little Nikita/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Little Nikita
+// Link to the problem: https://codeforces.com/contest/1977/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if (m <= n)
+    {
+        if ((n - m) % 2 == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
