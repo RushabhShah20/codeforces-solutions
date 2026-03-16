5587a5d8afcbd560b6e4009979a28199274686ee
Rushabh <20rush09abh2001@gmail.com>
2026-03-16 13:00:16 +0530

    Added C++ solution to 'Sale'

---
+++ Solutions/Sale/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Sale
+// Link to the problem: https://codeforces.com/contest/34/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    for (ll i = 0; i < m; i++)
+    {
+        if (a[i] <= 0)
+        {
+            ans -= a[i];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
