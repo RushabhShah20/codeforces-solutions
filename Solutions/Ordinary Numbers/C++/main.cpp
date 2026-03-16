b2a17dde961b031c62320aa6a97f967e5a8d77da
Rushabh <20rush09abh2001@gmail.com>
2026-03-16 16:18:59 +0530

    Added C++ solution to 'Ordinary Numbers'

---
+++ Solutions/Ordinary Numbers/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Ordinary Numbers
+// Link to the problem: https://codeforces.com/contest/1520/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size(), ans = 9 * (n - 1) + (stoll(s) / stoll(string(n, '1')));
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
