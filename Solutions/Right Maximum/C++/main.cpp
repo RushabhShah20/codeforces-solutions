c33653f3e11dfa8c71ccae48c48e37d0ab2ffc0f
Rushabh <20rush09abh2001@gmail.com>
2026-03-17 13:41:32 +0530

    Added C++ solution to 'Right Maximum'

---
+++ Solutions/Right Maximum/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Right Maximum
+// Link to the problem: https://codeforces.com/contest/2204/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, ans = 0, mx = 0;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        if (x >= mx)
+        {
+            mx = x;
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
