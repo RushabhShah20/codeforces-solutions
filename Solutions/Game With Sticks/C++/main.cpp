e73e864a39931e69aab5f37bd32e69886393bc37
Rushabh <20rush09abh2001@gmail.com>
2026-03-17 11:07:38 +0530

    Added C++ solution to 'Game With Sticks'

---
+++ Solutions/Game With Sticks/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Game With Sticks
+// Link to the problem: https://codeforces.com/contest/451/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if (min(m, n) % 2 == 0)
+    {
+        cout << "Malvika" << endl;
+    }
+    else
+    {
+        cout << "Akshat" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
