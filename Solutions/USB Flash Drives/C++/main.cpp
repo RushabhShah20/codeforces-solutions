eec3b9caebf950aaa9ce7a5962791e8b11f80325
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 13:50:06 +0530

    Added C++ solution to 'USB Flash Drives'

---
+++ Solutions/USB Flash Drives/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: USB Flash Drives
+// Link to the problem: https://codeforces.com/contest/609/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (m > 0)
+        {
+            m -= a[i];
+            ans++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
