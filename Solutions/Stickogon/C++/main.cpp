2a91ed6b182837984558ff00a462e20278756a05
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 17:17:50 +0530

    Added C++ solution to 'Stickogon'

---
+++ Solutions/Stickogon/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Stickogon
+// Link to the problem: https://codeforces.com/contest/1957/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    ll ans = 0;
+    for (const pair<ll, ll> i : m)
+    {
+        ans += (i.second / 3);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
