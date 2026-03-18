26f92819d0dcf5d9b764fc6120364ab8e8d4ed7b
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 19:18:40 +0530

    Added C++ solution to 'GamingForces'

---
+++ Solutions/GamingForces/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: GamingForces
+// Link to the problem: https://codeforces.com/contest/1792/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, y = 0;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        if (x == 1)
+        {
+            y++;
+        }
+    }
+    const ll ans = (n - y) + (y % 2) + (y / 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
