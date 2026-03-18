69f4cd2de60630dfeac3a18222ba19cc9d9ffd18
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 16:16:55 +0530

    Added C++ solution to 'Closest Point'

---
+++ Solutions/Closest Point/C++/main.cpp
@@ -0,0 +1,48 @@
+// Problem: Closest Point
+// Link to the problem: https://codeforces.com/contest/2004/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (n == 2)
+    {
+        if (abs(a[n - 1] - a[0]) > 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
