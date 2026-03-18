12f7f002c77e22d5df57d7aba8ce94dab166d4c9
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 15:58:12 +0530

    Added C++ solution to 'Sum in Binary Tree'

---
+++ Solutions/Sum in Binary Tree/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Sum in Binary Tree
+// Link to the problem: https://codeforces.com/contest/1843/problem/C
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = n;
+    while (n > 0)
+    {
+        ans += (n / 2);
+        n /= 2;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
