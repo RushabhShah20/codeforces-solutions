fa5ea5fe24c60bfbc8f9fdb67726caf665fd1e7d
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 17:05:12 +0530

    Added C++ solution to 'Find Divisible'

---
+++ Solutions/Find Divisible/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Find Divisible
+// Link to the problem: https://codeforces.com/contest/1096/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll l, r;
+    cin >> l >> r;
+    cout << l << " " << 2 * l << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
