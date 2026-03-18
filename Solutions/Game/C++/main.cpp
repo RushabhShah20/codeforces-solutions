458de5a5e7fee0e6ec080a6ccbff374418558eac
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 17:35:05 +0530

    Added C++ solution to 'Game'

---
+++ Solutions/Game/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Game
+// Link to the problem: https://codeforces.com/contest/984/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll ans = n % 2 == 0 ? a[(n - 1) / 2] : a[n / 2];
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
