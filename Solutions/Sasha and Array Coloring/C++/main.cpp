09f121875c92b3cc8284cf9fdb48e4798c52fbfa
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 16:31:18 +0530

    Added C++ solution to 'Sasha and Array Coloring'

---
+++ Solutions/Sasha and Array Coloring/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Sasha and Array Coloring
+// Link to the problem: https://codeforces.com/contest/1843/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0, i = 0, j = n - 1;
+    while (i < j)
+    {
+        ans += (a[j] - a[i]);
+        i++;
+        j--;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
