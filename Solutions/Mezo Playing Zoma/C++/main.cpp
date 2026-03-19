b6bf0c26add4d7cc94ead4bf5b01267cc88d0059
Rushabh <20rush09abh2001@gmail.com>
2026-03-19 13:48:55 +0530

    Added C++ solution to 'Mezo Playing Zoma'

---
+++ Solutions/Mezo Playing Zoma/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Mezo Playing Zoma
+// Link to the problem: https://codeforces.com/contest/1285/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    const ll ans = 1 + n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
