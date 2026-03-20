c332236e52f6e9bc789edc992f5557d0546c3b37
Rushabh <20rush09abh2001@gmail.com>
2026-03-20 15:13:02 +0530

    Added C++ solution to 'Telephone Number'

---
+++ Solutions/Telephone Number/C++/main.cpp
@@ -0,0 +1,46 @@
+// Problem: Telephone Number
+// Link to the problem: https://codeforces.com/contest/1167/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (n <= 10)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        ll x = s.find('8');
+        if (x != string::npos && x + 11 <= n)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
