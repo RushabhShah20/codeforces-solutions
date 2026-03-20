d56bb4458ae75da49300c12584d32c47ebfe4531
Rushabh <20rush09abh2001@gmail.com>
2026-03-20 16:31:14 +0530

    Added C++ solution to 'Two Vessels'

---
+++ Solutions/Two Vessels/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Two Vessels
+// Link to the problem: https://codeforces.com/contest/1872/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = (abs(a - b) + 2 * c - 1) / (2 * c);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
