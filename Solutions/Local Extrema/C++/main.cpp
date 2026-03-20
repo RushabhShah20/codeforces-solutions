b169e1581eb79cd74d11059108c918544161141b
Rushabh <20rush09abh2001@gmail.com>
2026-03-20 15:51:17 +0530

    Added C++ solution to 'Local Extrema'

---
+++ Solutions/Local Extrema/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Local Extrema
+// Link to the problem: https://codeforces.com/contest/888/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, ans = 0;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 1; i < n - 1; i++)
+    {
+        if ((a[i - 1] > a[i] && a[i] < a[i + 1]) || (a[i - 1] < a[i] && a[i] > a[i + 1]))
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
