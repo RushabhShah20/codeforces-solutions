737881e53d611180396c1f0e252b3e5e08768c5c
Rushabh <20rush09abh2001@gmail.com>
2026-03-20 14:09:43 +0530

    Added C++ solution to 'All Lengths Subtraction'

---
+++ Solutions/All Lengths Subtraction/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: All Lengths Subtraction
+// Link to the problem: https://codeforces.com/contest/2143/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 1; i < n - 1; i++)
+    {
+        if (a[i] < a[i - 1] && a[i] < a[i + 1])
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
