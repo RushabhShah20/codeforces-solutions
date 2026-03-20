89ca6b2429f37a6eab63615b3c63fa3ade41f398
Rushabh <20rush09abh2001@gmail.com>
2026-03-20 14:39:47 +0530

    Added C++ solution to 'Password'

---
+++ Solutions/Password/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Password
+// Link to the problem: https://codeforces.com/contest/1743/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    const ll ans = (10 - n) * (9 - n) * 3;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
