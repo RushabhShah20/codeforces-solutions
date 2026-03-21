5c1b003fb693eb8a3d361141396ce4fdff597a26
Rushabh <20rush09abh2001@gmail.com>
2026-03-21 16:55:54 +0530

    Added C++ solution to 'Upload More RAM'

---
+++ Solutions/Upload More RAM/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Upload More RAM
+// Link to the problem: https://codeforces.com/contest/1987/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = 1 + ((n - 1) * k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
