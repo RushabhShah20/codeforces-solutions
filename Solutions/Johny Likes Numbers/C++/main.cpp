bac156ee9640ca9243b54be903f93397acf63f1e
Rushabh <20rush09abh2001@gmail.com>
2026-03-26 15:10:29 +0530

    Added C++ solution to 'Johny Likes Numbers'

---
+++ Solutions/Johny Likes Numbers/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Johny Likes Numbers
+// Link to the problem: https://codeforces.com/contest/678/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = n + (k - n % k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
