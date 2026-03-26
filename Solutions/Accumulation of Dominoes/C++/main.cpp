7f9dadbf771f81002ef7df205682d8d54da9b8ff
Rushabh <20rush09abh2001@gmail.com>
2026-03-26 18:57:56 +0530

    Added C++ solution to 'Accumulation of Dominoes'

---
+++ Solutions/Accumulation of Dominoes/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Accumulation of Dominoes
+// Link to the problem: https://codeforces.com/contest/1725/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    const ll ans = (m == 1) ? (n - 1) : (n * (m - 1));
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
