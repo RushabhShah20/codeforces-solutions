fec08c4a7c9c9778d74675673e7f96891c2eb2d1
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 13:37:07 +0530

    Added C++ solution to 'Definite Game'

---
+++ Solutions/Definite Game/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Definite Game
+// Link to the problem: https://codeforces.com/contest/1081/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n == 2 ? 2 : 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
