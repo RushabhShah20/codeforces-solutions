9b5a6e565fcc0eaf9d52b64c1d5486b2aed58156
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 13:42:14 +0530

    Added C++ solution to 'Compote'

---
+++ Solutions/Compote/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Compote
+// Link to the problem: https://codeforces.com/contest/746/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = 7 * min({a, b / 2, c / 4});
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
