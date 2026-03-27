1034b83b07817caf27dfe09e82cca023f6268961
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 10:11:49 +0530

    Added C++ solution to 'Strange Functions'

---
+++ Solutions/Strange Functions/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Strange Functions
+// Link to the problem: https://codeforces.com/contest/1455/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << s.size() << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
