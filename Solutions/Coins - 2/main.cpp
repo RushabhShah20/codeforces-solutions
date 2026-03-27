22b92b8a5508c76ec3d9ee8da92c7657be4cc281
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 13:45:55 +0530

    Added C++ solution to 'Coins'

---
+++ Solutions/Coins - 2/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Coins
+// Link to the problem: https://codeforces.com/contest/1061/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = (n + k - 1) / n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
