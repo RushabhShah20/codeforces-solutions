237e686571077f0c2daad4af4b8fced4751f78c6
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 13:56:01 +0530

    Added C++ solution to 'Closing The Gap'

---
+++ Solutions/Closing The Gap/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Closing The Gap
+// Link to the problem: https://codeforces.com/contest/1615/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, y = 0;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        y += x;
+    }
+    const ll ans = y % n == 0 ? 0 : 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
