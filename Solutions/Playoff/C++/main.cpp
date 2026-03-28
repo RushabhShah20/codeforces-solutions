7e65992da82acf50d9e4be00bb93576357741974
Rushabh <20rush09abh2001@gmail.com>
2026-03-28 19:46:44 +0530

    Added C++ solution to 'Playoff'

---
+++ Solutions/Playoff/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Playoff
+// Link to the problem: https://codeforces.com/contest/1651/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (1 << n) - 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
