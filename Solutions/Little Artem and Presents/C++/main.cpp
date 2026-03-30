15d8e7306204fd63ad6eb3ae9a74017733a3a52f
Rushabh <20rush09abh2001@gmail.com>
2026-03-30 19:39:51 +0530

    Added C++ solution to 'Little Artem and Presents'

---
+++ Solutions/Little Artem and Presents/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Little Artem and Presents
+// Link to the problem: https://codeforces.com/contest/669/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n % 3 == 0 ? 2 * (n / 3) : 2 * (n / 3) + 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
