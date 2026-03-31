68ecb58da5acd2338a0d8697adb1c873a632d647
Rushabh <20rush09abh2001@gmail.com>
2026-03-31 17:37:16 +0530

    Added C++ solution to 'Park Lighting'

---
+++ Solutions/Park Lighting/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Park Lighting
+// Link to the problem: https://codeforces.com/contest/1358/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    const ll ans = (m * n + 1) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
