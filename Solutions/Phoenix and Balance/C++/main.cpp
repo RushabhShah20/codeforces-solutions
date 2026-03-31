218dfc2650ad5798c44eadb8b36a4f21dba206a5
Rushabh <20rush09abh2001@gmail.com>
2026-03-31 17:49:16 +0530

    Added C++ solution to 'Phoenix and Balance'

---
+++ Solutions/Phoenix and Balance/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Phoenix and Balance
+// Link to the problem: https://codeforces.com/contest/1348/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (1 << (n / 2 + 1)) - 2 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
