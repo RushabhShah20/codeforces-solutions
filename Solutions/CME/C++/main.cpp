50de622be455503be1038e3717b974090c4891d9
Rushabh <20rush09abh2001@gmail.com>
2026-03-31 20:46:21 +0530

    Added C++ solution to 'CME'

---
+++ Solutions/CME/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: CME
+// Link to the problem: https://codeforces.com/contest/1223/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n <= 4)
+    {
+        cout << 4 - n << endl;
+    }
+    else
+    {
+        cout << n % 2 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
