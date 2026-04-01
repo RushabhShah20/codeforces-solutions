fc84e16713e67dd549ea1b33f35db986edc0b92e
Rushabh <20rush09abh2001@gmail.com>
2026-04-01 17:27:30 +0530

    Added C++ solution to 'Simply Sitting on Chairs'

---
+++ Solutions/Simply Sitting on Chairs/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Simply Sitting on Chairs
+// Link to the problem: https://codeforces.com/contest/2210/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        if (x <= i + 1)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
