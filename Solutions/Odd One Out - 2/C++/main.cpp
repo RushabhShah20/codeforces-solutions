00ffce010f9b5098398c92d3066e2c8f6215930f
Rushabh <20rush09abh2001@gmail.com>
2026-04-01 22:10:25 +0530

    Added C++ solution to 'Odd One Out'

---
+++ Solutions/Odd One Out - 2/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Odd One Out
+// Link to the problem: https://codeforces.com/contest/2214/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    cout << "C2" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
