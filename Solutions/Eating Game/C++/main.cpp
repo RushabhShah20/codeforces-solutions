4ac257cff24b3b6f89f5590ea4e4e7d77b6fca87
Rushabh <20rush09abh2001@gmail.com>
2026-04-02 14:15:08 +0530

    Added C++ solution to 'Eating Game'

---
+++ Solutions/Eating Game/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Eating Game
+// Link to the problem: https://codeforces.com/contest/2200/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    ll mx = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mx = max(mx, a[i]);
+    }
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] == mx)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
