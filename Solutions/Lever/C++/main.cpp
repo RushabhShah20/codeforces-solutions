7a2d5217ef4f118e143a4f639d6901623cadcf6e
Rushabh <20rush09abh2001@gmail.com>
2026-04-02 15:11:21 +0530

    Added C++ solution to 'Lever'

---
+++ Solutions/Lever/C++/main.cpp
@@ -0,0 +1,46 @@
+// Problem: Lever
+// Link to the problem: https://codeforces.com/contest/2131/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    ll ans = 1;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] > b[i])
+        {
+            ans += a[i] - b[i];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
