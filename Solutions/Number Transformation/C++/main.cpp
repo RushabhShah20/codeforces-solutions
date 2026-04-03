41b3a1efcfb51ec5d9917881e4a048b82bdccc55
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 18:28:32 +0530

    Added C++ solution to 'Number Transformation'

---
+++ Solutions/Number Transformation/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Number Transformation
+// Link to the problem: https://codeforces.com/contest/1674/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    if (!(y % x))
+    {
+        cout << 1 << " " << y / x << endl;
+    }
+    else
+    {
+        cout << 0 << " " << 0 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
