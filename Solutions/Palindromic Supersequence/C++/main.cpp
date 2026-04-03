8a77ceb8f1d893b981bed2db3515ea7df570ce4a
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 11:33:29 +0530

    Added C++ solution to 'Palindromic Supersequence'

---
+++ Solutions/Palindromic Supersequence/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Palindromic Supersequence
+// Link to the problem: https://codeforces.com/contest/932/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    string t = s;
+    reverse(t.begin(), t.end());
+    const string ans = s + t;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
