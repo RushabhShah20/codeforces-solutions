dba2e153699ef18178bf1c1f5fb08d52ac324a33
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 18:52:20 +0530

    Added C++ solution to 'Yet Another Dividing into Teams'

---
+++ Solutions/Yet Another Dividing into Teams/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Yet Another Dividing into Teams
+// Link to the problem: https://codeforces.com/contest/1249/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] - a[i - 1] == 1)
+        {
+            cout << 2 << endl;
+            return;
+        }
+    }
+    cout << 1 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
