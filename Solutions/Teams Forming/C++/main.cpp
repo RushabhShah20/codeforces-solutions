8a12e11e6e56372323f5e2c9218f164b7f800b7e
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 15:29:47 +0530

    Added C++ solution to 'Teams Forming'

---
+++ Solutions/Teams Forming/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Teams Forming
+// Link to the problem: https://codeforces.com/contest/1092/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    for (ll i = 0; i < n; i += 2)
+    {
+        ans += a[i + 1] - a[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
