b60c48d13b4e98afcf85ae455a7d24a34eee35fd
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 19:09:51 +0530

    Added C++ solution to 'Sakurako and Kosuke'

---
+++ Solutions/Sakurako and Kosuke/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Sakurako and Kosuke
+// Link to the problem: https://codeforces.com/contest/2033/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n & 1)
+    {
+        cout << "Kosuke" << endl;
+    }
+    else
+    {
+        cout << "Sakurako" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
