881907772582f164ac68759a5d7f29da5c0e191a
Rushabh <20rush09abh2001@gmail.com>
2026-04-03 17:54:42 +0530

    Added C++ solution to 'Bun Lover'

---
+++ Solutions/Bun Lover/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Bun Lover
+// Link to the problem: https://codeforces.com/contest/1822/problem/C
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n * n + 2 * (n + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
