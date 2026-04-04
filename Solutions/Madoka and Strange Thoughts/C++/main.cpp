f21d4800f054ea075050fc8adc3f6763c1b11652
Rushabh <20rush09abh2001@gmail.com>
2026-04-04 15:12:02 +0530

    Added C++ solution to 'Madoka and Strange Thoughts'

---
+++ Solutions/Madoka and Strange Thoughts/C++/main.cpp
@@ -0,0 +1,34 @@
+// Problem: Madoka and Strange Thoughts
+// Link to the problem: https://codeforces.com/contest/1717/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 2 * (n + n / 3);
+    if (n & 1)
+    {
+        ans--;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
