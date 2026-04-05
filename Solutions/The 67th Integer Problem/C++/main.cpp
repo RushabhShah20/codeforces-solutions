9f9e222d54feecca528b59ff310fe842bad5ea6d
Rushabh <20rush09abh2001@gmail.com>
2026-04-05 20:12:24 +0530

    Added C++ solution to 'The 67th Integer Problem'

---
+++ Solutions/The 67th Integer Problem/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: The 67th Integer Problem
+// Link to the problem: https://codeforces.com/contest/2218/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    cout << x << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
