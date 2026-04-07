845d33498be44b117719ca161b3f5d7ded2b836b
Rushabh <20rush09abh2001@gmail.com>
2026-04-07 15:38:00 +0530

    Added C++ solution to 'The 67th XOR Problem'

---
+++ Solutions/The 67th XOR Problem/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: The 67th XOR Problem
+// Link to the problem: https://codeforces.com/contest/2218/problem/E
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = i + 1; j < n; j++)
+        {
+            ans = max(ans, a[i] ^ a[j]);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
