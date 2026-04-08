13a7a9c6dfff231ac6eb06b25da78f2f1855047b
Rushabh <20rush09abh2001@gmail.com>
2026-04-08 16:21:17 +0530

    Added C++ solution to 'Optimal Point on a Line'

---
+++ Solutions/Optimal Point on a Line/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Optimal Point on a Line
+// Link to the problem: https://codeforces.com/contest/710/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    if (n % 2 == 0)
+    {
+        cout << a[(n - 1) / 2] << endl;
+    }
+    else
+    {
+        cout << a[n / 2] << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
