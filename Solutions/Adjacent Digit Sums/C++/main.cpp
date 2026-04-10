e7dc617de449324c02327e7e301ea81079c51dce
Rushabh <20rush09abh2001@gmail.com>
2026-04-10 19:07:02 +0530

    Added C++ solution to 'Adjacent Digit Sums'

---
+++ Solutions/Adjacent Digit Sums/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Adjacent Digit Sums
+// Link to the problem: https://codeforces.com/contest/2067/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    if (y <= x + 1 && (x + 1 - y) % 9 == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
