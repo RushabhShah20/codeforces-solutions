5be554b803e5a63999c0f981f210d5ff93bcfb23
Rushabh <20rush09abh2001@gmail.com>
2026-04-12 11:52:09 +0530

    Added C++ solution to 'Specific Tastes of Andre'

---
+++ Solutions/Specific Tastes of Andre/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Specific Tastes of Andre
+// Link to the problem: https://codeforces.com/contest/1438/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << 1 << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
