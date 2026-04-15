47b97a3fb1e6ceff022dec3c983d125939df54d5
Rushabh <20rush09abh2001@gmail.com>
2026-04-15 18:44:49 +0530

    Added C++ solution to 'Remove a Progression'

---
+++ Solutions/Remove a Progression/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Remove a Progression
+// Link to the problem: https://codeforces.com/contest/1194/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    cout << 2 * x << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
