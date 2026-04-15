a6e54f968107ff4f456c471812793184e84c4b74
Rushabh <20rush09abh2001@gmail.com>
2026-04-15 15:36:55 +0530

    Added C++ solution to 'Coin Transformation'

---
+++ Solutions/Coin Transformation/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Coin Transformation
+// Link to the problem: https://codeforces.com/contest/2043/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 1;
+    while (n > 3)
+    {
+        ans *= 2;
+        n /= 4;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
