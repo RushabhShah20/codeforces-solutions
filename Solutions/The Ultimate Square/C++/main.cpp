affe0953445fca3c9c97f83061a45742ed109d87
Rushabh <20rush09abh2001@gmail.com>
2026-04-15 17:04:46 +0530

    Added C++ solution to 'The Ultimate Square'

---
+++ Solutions/The Ultimate Square/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: The Ultimate Square
+// Link to the problem: https://codeforces.com/contest/1748/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n & 1)
+    {
+        cout << (n + 1) / 2 << endl;
+    }
+    else
+    {
+        cout << n / 2 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
