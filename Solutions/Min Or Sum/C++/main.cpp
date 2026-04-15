f187afbdfbd495de09c29354fcac5febdd6674a5
Rushabh <20rush09abh2001@gmail.com>
2026-04-15 16:53:01 +0530

    Added C++ solution to 'Min Or Sum'

---
+++ Solutions/Min Or Sum/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Min Or Sum
+// Link to the problem: https://codeforces.com/contest/1635/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ans |= x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
