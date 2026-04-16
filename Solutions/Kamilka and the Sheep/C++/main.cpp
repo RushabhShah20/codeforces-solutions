c5ffd72c5a031bccf187dc652b2516648c099d98
Rushabh <20rush09abh2001@gmail.com>
2026-04-16 18:22:44 +0530

    Added C++ solution to 'Kamilka and the Sheep'

---
+++ Solutions/Kamilka and the Sheep/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Kamilka and the Sheep
+// Link to the problem: https://codeforces.com/contest/2092/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, mn = LLONG_MAX, mx = LLONG_MIN;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        mn = min(mn, x);
+        mx = max(mx, x);
+    }
+    const ll ans = mx - mn;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
