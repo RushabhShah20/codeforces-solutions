d0118656aee02464a8209d96b5283b9efa61e06a
Rushabh <20rush09abh2001@gmail.com>
2026-04-16 16:46:55 +0530

    Added C++ solution to 'Perfect Permutation'

---
+++ Solutions/Perfect Permutation - 2/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Perfect Permutation
+// Link to the problem: https://codeforces.com/contest/1711/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> ans(n);
+    ans[0] = n;
+    for (ll i = 1; i < n; i++)
+    {
+        ans[i] = i;
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
