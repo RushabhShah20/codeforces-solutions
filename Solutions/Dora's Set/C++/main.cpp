5bc52c4a9523d93e8757ed1f159ce4c9279aa904
Rushabh <20rush09abh2001@gmail.com>
2026-04-17 15:37:24 +0530

    Added C++ solution to 'Dora's Set'

---
+++ Solutions/Dora's Set/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Dora's Set
+// Link to the problem: https://codeforces.com/contest/2007/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll l, r;
+    cin >> l >> r;
+    const ll ans = ((r & 1 ? r + 1 : r) - (l & 1 ? l - 1 : l)) / 4;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
