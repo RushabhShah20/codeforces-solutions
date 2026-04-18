e1b1be90249f6433ca481f2ebb27de6e9d6f69d6
Rushabh <20rush09abh2001@gmail.com>
2026-04-18 15:54:48 +0530

    Added C++ solution to 'Permutation Warm-Up'

---
+++ Solutions/Permutation Warm-Up/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Permutation Warm-Up
+// Link to the problem: https://codeforces.com/contest/2108/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = 1 + n / 2 * ((n + 1) / 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
