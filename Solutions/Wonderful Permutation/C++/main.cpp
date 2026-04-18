84225e3c676c8907b71f9549652a8fa43366c34a
Rushabh <20rush09abh2001@gmail.com>
2026-04-18 15:26:29 +0530

    Added C++ solution to 'Wonderful Permutation'

---
+++ Solutions/Wonderful Permutation/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Wonderful Permutation
+// Link to the problem: https://codeforces.com/contest/1712/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (ll i = 0; i < k; i++)
+    {
+        if (a[i] > k)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
