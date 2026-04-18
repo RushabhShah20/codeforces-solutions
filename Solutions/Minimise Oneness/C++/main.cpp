5ac93d80e0a1a5feedcf42b72e358ba33f34dfcc
Rushabh <20rush09abh2001@gmail.com>
2026-04-18 16:02:19 +0530

    Added C++ solution to 'Minimise Oneness'

---
+++ Solutions/Minimise Oneness/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Minimise Oneness
+// Link to the problem: https://codeforces.com/contest/2030/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string ans(n, '0');
+    ans[n / 2] = '1';
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
