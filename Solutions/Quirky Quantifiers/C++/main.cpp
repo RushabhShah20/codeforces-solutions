c5f5edda4cab3e0537eee0b17ce63162e8175960
Rushabh <20rush09abh2001@gmail.com>
2026-04-20 16:13:32 +0530

    Added C++ solution to 'Quirky Quantifiers'

---
+++ Solutions/Quirky Quantifiers/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Quirky Quantifiers
+// Link to the problem: https://codeforces.com/contest/952/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n & 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
