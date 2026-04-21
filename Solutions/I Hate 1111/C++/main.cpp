6acf5cf65c7bef9272b2dc70c3973463f408ad4c
Rushabh <20rush09abh2001@gmail.com>
2026-04-21 16:23:30 +0530

    Added C++ solution to 'I Hate 1111'

---
+++ Solutions/I Hate 1111/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: I Hate 1111
+// Link to the problem: https://codeforces.com/contest/1526/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n >= 111 * (n % 11))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
