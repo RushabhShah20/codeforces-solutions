3ac3989a28e0466dc3f81465aec3cdbd29a34d13
Rushabh <20rush09abh2001@gmail.com>
2026-04-23 16:49:51 +0530

    Added C++ solution to 'PizzaForces'

---
+++ Solutions/PizzaForces/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: PizzaForces
+// Link to the problem: https://codeforces.com/contest/1555/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    if (n <= 6)
+    {
+        ans = 15;
+    }
+    else
+    {
+        ans = (n + (n & 1)) * 5 / 2;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
