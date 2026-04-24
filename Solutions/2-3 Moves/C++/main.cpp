81764e4aa0e3f25c1b5c9861de7e6ea75ab7fa40
Rushabh <20rush09abh2001@gmail.com>
2026-04-24 17:33:41 +0530

    Added C++ solution to '2-3 Moves'

---
+++ Solutions/2-3 Moves/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: 2-3 Moves
+// Link to the problem: https://codeforces.com/contest/1716/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n == 1 ? 2 : (n + 2) / 3;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
