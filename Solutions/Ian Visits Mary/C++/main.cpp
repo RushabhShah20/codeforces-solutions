c660c8e53e6f15a5e606e952a9c0f36a90a218b2
Rushabh <20rush09abh2001@gmail.com>
2026-04-25 19:56:06 +0530

    Added C++ solution to 'Ian Visits Mary'

---
+++ Solutions/Ian Visits Mary/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Ian Visits Mary
+// Link to the problem: https://codeforces.com/contest/1816/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << 2 << endl;
+    cout << a - 1 << " " << 1 << endl;
+    cout << a << " " << b << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
