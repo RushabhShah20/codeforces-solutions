5b552b34250cdd93b39b0b0b654c6350477da0ed
Rushabh <20rush09abh2001@gmail.com>
2026-04-25 19:20:09 +0530

    Added C++ solution to 'A.M. Deviation'

---
+++ Solutions/A.M. Deviation/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: A.M. Deviation
+// Link to the problem: https://codeforces.com/contest/1605/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a1, a2, a3;
+    cin >> a1 >> a2 >> a3;
+    cout << (abs(a1 + a3 - 2 * a2) % 3 == 0 ? 0 : 1) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
