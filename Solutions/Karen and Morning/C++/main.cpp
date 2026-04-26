e6e670affa72f92bad30ab65992ec091914c0bcc
Rushabh <20rush09abh2001@gmail.com>
2026-04-26 18:18:42 +0530

    Added C++ solution to 'Karen and Morning'

---
+++ Solutions/Karen and Morning/C++/main.cpp
@@ -0,0 +1,47 @@
+// Problem: Karen and Morning
+// Link to the problem: https://codeforces.com/contest/816/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+ll minutes(const string &time)
+{
+    const string hh = time.substr(0, 2), mm = time.substr(3, 2);
+    const ll h = stoi(hh), m = stoi(mm);
+    return h * 60 + m;
+}
+
+void solve()
+{
+    const vector<string> times = {"00:00", "01:10", "02:20", "03:30", "04:40", "05:50", "10:01", "11:11", "12:21", "13:31", "14:41", "15:51", "20:02", "21:12", "22:22", "23:32"};
+    string s;
+    cin >> s;
+    vector<ll> mins;
+    for (const string &time : times)
+    {
+        mins.push_back(minutes(time));
+    }
+    const ll x = minutes(s);
+    ll ans = LLONG_MAX;
+    for (const ll i : mins)
+    {
+        if (i >= x)
+        {
+            ans = min(ans, i - x);
+        }
+    }
+    ans = min(ans, 1440 - x);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
