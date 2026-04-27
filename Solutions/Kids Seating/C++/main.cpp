ec5dce4c0547a522df3a0aab07ee91c02da28cca
Rushabh <20rush09abh2001@gmail.com>
2026-04-27 15:55:41 +0530

    Added C++ solution to 'Kids Seating'

---
+++ Solutions/Kids Seating/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Kids Seating
+// Link to the problem: https://codeforces.com/contest/1443/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll x = 4 * n;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << x << " ";
+        x -= 2;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
