fadca0da9df1ce42c58129fd687768ace8f2d75c
Rushabh <20rush09abh2001@gmail.com>
2026-04-27 15:45:52 +0530

    Added C++ solution to 'Avoid Trygub'

---
+++ Solutions/Avoid Trygub/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Avoid Trygub
+// Link to the problem: https://codeforces.com/contest/1450/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
