7632750b840de4b33b343fef8efdc8b45fa413db
Rushabh <20rush09abh2001@gmail.com>
2026-04-28 17:32:11 +0530

    Added C++ solution to 'Preparing for the Contest'

---
+++ Solutions/Preparing for the Contest/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Preparing for the Contest
+// Link to the problem: https://codeforces.com/contest/1914/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    for (ll i = 1; i <= k; i++)
+    {
+        cout << i << " ";
+    }
+    for (ll i = n; i > k; i--)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
