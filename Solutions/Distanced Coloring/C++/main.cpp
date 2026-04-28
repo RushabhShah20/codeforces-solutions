8e94bd5392ddc1ce1b451fcff8196330ba03116f
Rushabh <20rush09abh2001@gmail.com>
2026-04-28 16:31:13 +0530

    Added C++ solution to 'Distanced Coloring'

---
+++ Solutions/Distanced Coloring/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Distanced Coloring
+// Link to the problem: https://codeforces.com/contest/2002/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    const ll ans = min(n, k) * min(m, k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
