df3464c3805e2e703b56d35c61a103fd07c9c814
Rushabh <20rush09abh2001@gmail.com>
2026-04-29 19:25:13 +0530

    Added C++ solution to 'Digit Minimization'

---
+++ Solutions/Digit Minimization/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Digit Minimization
+// Link to the problem: https://codeforces.com/contest/1684/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll n = s.size();
+    if (n == 2)
+    {
+        cout << s[1] - '0' << endl;
+    }
+    else
+    {
+        sort(s.begin(), s.end());
+        cout << s[0] - '0' << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
