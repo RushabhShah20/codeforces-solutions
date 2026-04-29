365b9e74902000f7cba1a74096952e8c94e28de1
Rushabh <20rush09abh2001@gmail.com>
2026-04-29 18:33:34 +0530

    Added C++ solution to 'Sasha and Sticks'

---
+++ Solutions/Sasha and Sticks/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Sasha and Sticks
+// Link to the problem: https://codeforces.com/contest/832/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n / k & 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
