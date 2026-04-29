993f1c2a00c50edd23e63d9c9d7f50a8130c7cfa
Rushabh <20rush09abh2001@gmail.com>
2026-04-29 17:36:02 +0530

    Added C++ solution to 'Robot Program'

---
+++ Solutions/Robot Program/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Robot Program
+// Link to the problem: https://codeforces.com/contest/1452/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    const ll ans = abs(x - y) <= 1 ? x + y : 2 * max(x, y) - 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
