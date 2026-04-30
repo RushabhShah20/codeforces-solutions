911cadeab723ee32d220b0a5a8ac1497408e644f
Rushabh <20rush09abh2001@gmail.com>
2026-04-30 14:12:22 +0530

    Added C++ solution to 'Two Frogs'

---
+++ Solutions/Two Frogs/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Two Frogs
+// Link to the problem: https://codeforces.com/contest/2055/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    if (abs(a - b) & 1)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
