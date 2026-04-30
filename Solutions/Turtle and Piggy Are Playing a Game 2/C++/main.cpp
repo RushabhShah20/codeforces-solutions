d2c760be7e69dade878f357f4cdc9a88d8406f6f
Rushabh <20rush09abh2001@gmail.com>
2026-04-30 14:22:26 +0530

    Added C++ solution to 'Turtle and Piggy Are Playing a Game 2'

---
+++ Solutions/Turtle and Piggy Are Playing a Game 2/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Turtle and Piggy Are Playing a Game 2
+// Link to the problem: https://codeforces.com/contest/2003/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll ans = a[n / 2];
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
