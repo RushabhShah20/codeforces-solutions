0f7392eeb74b7a7f609ed5e0d1ad5bf178ac3d50
Rushabh <20rush09abh2001@gmail.com>
2026-05-01 22:26:59 +0530

    Added C++ solution to 'Squares and Cubes'

---
+++ Solutions/Squares and Cubes/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Squares and Cubes
+// Link to the problem: https://codeforces.com/contest/1619/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll x = sqrtl(n), y = cbrtl(n), z = sqrtl(cbrtl(n));
+    const ll ans = x + y - z;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
