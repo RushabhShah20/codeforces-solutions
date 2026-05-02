abf9c68f7dfa90129276d86c25ccfd0d4506c7b5
Rushabh <20rush09abh2001@gmail.com>
2026-05-02 16:56:19 +0530

    Added C++ solution to 'Treasure Hunt'

---
+++ Solutions/Treasure Hunt/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Treasure Hunt
+// Link to the problem: https://codeforces.com/contest/2090/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    z %= x + y;
+    if (z < x)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
