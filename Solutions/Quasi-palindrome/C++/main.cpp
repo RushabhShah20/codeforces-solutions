40591ccfe47d641bfe3051428e961aeb3d504342
Rushabh <20rush09abh2001@gmail.com>
2026-05-03 22:02:57 +0530

    Added C++ solution to 'Quasi-palindrome'

---
+++ Solutions/Quasi-palindrome/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Quasi-palindrome
+// Link to the problem: https://codeforces.com/contest/863/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    while (!s.empty() && s.back() == '0')
+    {
+        s.pop_back();
+    }
+    string t = s;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
