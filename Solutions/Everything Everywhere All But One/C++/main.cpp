34a39dd4405b47b3a450efdb7030b2491d3f94e1
Rushabh <20rush09abh2001@gmail.com>
2026-05-05 02:43:34 +0530

    Added C++ solution to 'Everything Everywhere All But One'

---
+++ Solutions/Everything Everywhere All But One/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Everything Everywhere All But One
+// Link to the problem: https://codeforces.com/contest/1686/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        x += a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        if (x - a[i] == (n - 1) * a[i])
+        {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
