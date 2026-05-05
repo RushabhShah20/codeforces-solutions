25b9012876d3e2e34568f26d7acbe2e7a543509d
Rushabh <20rush09abh2001@gmail.com>
2026-05-05 17:53:50 +0530

    Added C++ solution to 'Splits'

---
+++ Solutions/Splits/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Splits
+// Link to the problem: https://codeforces.com/contest/964/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n + 2) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
