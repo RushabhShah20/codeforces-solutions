85af450bfc0938bfbde7a3063418a9dbf9b74b47
Rushabh <20rush09abh2001@gmail.com>
2026-05-07 15:26:12 +0530

    Added C++ solution to 'Last minute enhancements'

---
+++ Solutions/Last minute enhancements/C++/main.cpp
@@ -0,0 +1,45 @@
+// Problem: Last minute enhancements
+// Link to the problem: https://codeforces.com/contest/1466/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    for (pair<ll, ll> i : m)
+    {
+        if (i.second > 1)
+        {
+            i.second--;
+            m[i.first + 1]++;
+        }
+    }
+    const ll ans = m.size();
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
