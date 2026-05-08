874b4877c1e3b40f09fa664424032a6fd1a2434f
Rushabh <20rush09abh2001@gmail.com>
2026-05-08 15:00:13 +0530

    Added C++ solution to 'Squares'

---
+++ Solutions/Squares/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Squares
+// Link to the problem: https://codeforces.com/contest/263/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    if (k > n)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    cout << a[k - 1] << " " << 0 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
