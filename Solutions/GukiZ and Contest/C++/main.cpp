0e9f06b3a7d11d8a04fec31c42c667908a2198a3
Rushabh <20rush09abh2001@gmail.com>
2026-05-09 15:42:56 +0530

    Added C++ solution to 'GukiZ and Contest'

---
+++ Solutions/GukiZ and Contest/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: GukiZ and Contest
+// Link to the problem: https://codeforces.com/contest/551/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), ans(n, 1);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = 0; j < n; j++)
+        {
+            if (a[j] > a[i])
+            {
+                ans[i]++;
+            }
+        }
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
