d81346b17ad4b413bbff6a589ba7da8c8009e026
Rushabh <20rush09abh2001@gmail.com>
2026-05-09 18:11:40 +0530

    Added C++ solution to 'Another One Bites The Dust'

---
+++ Solutions/Another One Bites The Dust/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Another One Bites The Dust
+// Link to the problem: https://codeforces.com/contest/1148/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = 2 * c + (a == b ? 2 * a : 2 * min(a, b) + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
