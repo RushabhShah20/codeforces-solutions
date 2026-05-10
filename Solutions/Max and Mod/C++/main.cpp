2889979594d5918b45206d249af30c616033e012
Rushabh <20rush09abh2001@gmail.com>
2026-05-10 16:47:01 +0530

    Added C++ solution to 'Max and Mod'

---
+++ Solutions/Max and Mod/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Max and Mod
+// Link to the problem: https://codeforces.com/contest/2084/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n & 1)
+    {
+        cout << n << " ";
+        for (ll i = 1; i < n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
