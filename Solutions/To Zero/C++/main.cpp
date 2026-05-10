cf548c4a08d536b7abe5ae2eb4d5c8af1b270a7b
Rushabh <20rush09abh2001@gmail.com>
2026-05-10 18:37:18 +0530

    Added C++ solution to 'To Zero'

---
+++ Solutions/To Zero/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: To Zero
+// Link to the problem: https://codeforces.com/contest/2075/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = n & 1 ? 1 + (n - 2) / (k - 1) : (n + k - 2) / (k - 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
