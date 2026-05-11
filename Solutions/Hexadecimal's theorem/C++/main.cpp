243402e7f4b44cdb308c69783d3cb3455f410db5
Rushabh <20rush09abh2001@gmail.com>
2026-05-11 15:01:44 +0530

    Added C++ solution to 'Hexadecimal's theorem'

---
+++ Solutions/Hexadecimal's theorem/C++/main.cpp
@@ -0,0 +1,24 @@
+// Problem: Hexadecimal's theorem
+// Link to the problem: https://codeforces.com/contest/199/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << "0 0 " << n << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
