ac02e6289ffbece3a21298749ca4b0b50c47c18d
Rushabh <20rush09abh2001@gmail.com>
2026-05-11 18:22:19 +0530

    Added C++ solution to 'The number of positions'

---
+++ Solutions/The number of positions/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: The number of positions
+// Link to the problem: https://codeforces.com/contest/124/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    const ll ans = min(n - a, b + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
