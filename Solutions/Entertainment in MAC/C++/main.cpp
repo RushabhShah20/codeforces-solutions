b3f88c95a9fb6db07c88f9581b212e14711b76c2
Rushabh <20rush09abh2001@gmail.com>
2026-05-13 17:39:12 +0530

    Added C++ solution to 'Entertainment in MAC'

---
+++ Solutions/Entertainment in MAC/C++/main.cpp
@@ -0,0 +1,34 @@
+// Problem: Entertainment in MAC
+// Link to the problem: https://codeforces.com/contest/1935/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string t = s;
+    reverse(t.begin(), t.end());
+    const string ans = s <= t ? s : t + s;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
