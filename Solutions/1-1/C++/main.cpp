968f709f041b654a29ec33ccb8be1ce2f648c531
Rushabh <20rush09abh2001@gmail.com>
2026-05-14 20:05:45 +0530

    Added C++ solution to '1-1'

---
+++ Solutions/1-1/C++/main.cpp
@@ -0,0 +1,61 @@
+// Problem: 1-1
+// Link to the problem: https://codeforces.com/contest/2207/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (ll i = 1; i < n - 1; i++)
+    {
+        if (s[i - 1] == '1' && s[i + 1] == '1')
+        {
+            s[i] = '1';
+        }
+    }
+    ll y = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            y++;
+        }
+    }
+    for (ll i = 1; i < n - 1; i++)
+    {
+        if (s[i - 1] == '1' && s[i + 1] == '1')
+        {
+            s[i] = '0';
+        }
+    }
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            x++;
+        }
+    }
+    cout << x << " " << y << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
