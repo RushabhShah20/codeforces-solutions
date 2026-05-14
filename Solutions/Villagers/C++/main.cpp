724e03ae0bf41bede4928e981380d7922bc43605
Rushabh <20rush09abh2001@gmail.com>
2026-05-14 17:12:47 +0530

    Added C++ solution to 'Villagers'

---
+++ Solutions/Villagers/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Villagers
+// Link to the problem: https://codeforces.com/contest/2133/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    ll ans = 0;
+    for (ll i = 0; i < n; i += 2)
+    {
+        ans += a[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
