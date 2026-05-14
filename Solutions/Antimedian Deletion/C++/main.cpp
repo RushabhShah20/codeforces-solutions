ad6fdc8b20e5643e73cdab84870745325691ea4b
Rushabh <20rush09abh2001@gmail.com>
2026-05-14 16:37:28 +0530

    Added C++ solution to 'Antimedian Deletion'

---
+++ Solutions/Antimedian Deletion/C++/main.cpp
@@ -0,0 +1,45 @@
+// Problem: Antimedian Deletion
+// Link to the problem: https://codeforces.com/contest/2211/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (n == 1)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        for (ll i = 0; i < n; i++)
+        {
+            cout << 2 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
