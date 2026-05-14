3d708156c871796836e4e3a802fc45fbb148fc23
Rushabh <20rush09abh2001@gmail.com>
2026-05-14 17:04:13 +0530

    Added C++ solution to 'Deletion Sort'

---
+++ Solutions/Deletion Sort/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Deletion Sort
+// Link to the problem: https://codeforces.com/contest/2200/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            cout << 1 << endl;
+            return;
+        }
+    }
+    cout << n << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
