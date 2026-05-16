6c232205daca41b538b523c1f11c5e5a0b46d8ff
Rushabh <20rush09abh2001@gmail.com>
2026-05-16 17:31:05 +0530

    Added C++ solution to 'Planets'

---
+++ Solutions/Planets/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Planets
+// Link to the problem: https://codeforces.com/contest/1730/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    ll ans = 0;
+    for (const pair<ll, ll> i : m)
+    {
+        ans += min(i.second, k);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
