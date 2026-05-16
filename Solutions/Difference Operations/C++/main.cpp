53b60ff41a4d70b88dbba3efc7d170e0f70aa54b
Rushabh <20rush09abh2001@gmail.com>
2026-05-16 17:39:53 +0530

    Added C++ solution to 'Difference Operations'

---
+++ Solutions/Difference Operations/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Difference Operations
+// Link to the problem: https://codeforces.com/contest/1708/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll g = a[1];
+    for (ll i = 2; i < n; i++)
+    {
+        g = __gcd(g, a[i]);
+    }
+    const string ans = g % a[0] == 0 ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
