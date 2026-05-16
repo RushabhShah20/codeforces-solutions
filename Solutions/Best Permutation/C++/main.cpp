7daa9825eee8bd3267ee71da025fca9c5feeeede
Rushabh <20rush09abh2001@gmail.com>
2026-05-16 17:23:34 +0530

    Added C++ solution to 'Best Permutation'

---
+++ Solutions/Best Permutation/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Best Permutation
+// Link to the problem: https://codeforces.com/contest/1728/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        a[i] = i + 1;
+    }
+    for (ll i = n & 1 ? 1 : 0; i < n - 2; i += 2)
+    {
+        swap(a[i], a[i + 1]);
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
