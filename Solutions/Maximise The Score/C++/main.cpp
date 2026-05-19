2f9868eea459b63e8324e40b54e29a68b78100e4
Rushabh <20rush09abh2001@gmail.com>
2026-05-19 20:32:01 +0530

    Added C++ solution to 'Maximise The Score'

---
+++ Solutions/Maximise The Score/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Maximise The Score
+// Link to the problem: https://codeforces.com/contest/1930/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(2 * n);
+    for (ll i = 0; i < 2 * n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    for (ll i = 0; i < 2 * n; i += 2)
+    {
+        ans += a[i];
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
