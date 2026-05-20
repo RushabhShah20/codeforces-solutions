b77a361cb5ba4a4bcf29587986a5cfd17107da54
Rushabh <20rush09abh2001@gmail.com>
2026-05-20 18:12:18 +0530

    Added C++ solution to 'New Year and Naming'

---
+++ Solutions/New Year and Naming/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: New Year and Naming
+// Link to the problem: https://codeforces.com/contest/1284/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<string> a(n), b(m);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    ll k;
+    cin >> k;
+    for (ll i = 0; i < k; i++)
+    {
+        ll j;
+        cin >> j;
+        const string ans = a[(j - 1) % n] + b[(j - 1) % m];
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
