b67fc4ac8251b8d21d38705fcd14b57eb5e483ed
Rushabh <20rush09abh2001@gmail.com>
2026-05-20 17:51:39 +0530

    Added C++ solution to 'Kuroni and the Gifts'

---
+++ Solutions/Kuroni and the Gifts/C++/main.cpp
@@ -0,0 +1,49 @@
+// Problem: Kuroni and the Gifts
+// Link to the problem: https://codeforces.com/contest/1305/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
