28c7ecb44a407056ac0170de0a118e3f6e1e2d51
Rushabh <20rush09abh2001@gmail.com>
2026-05-22 16:02:29 +0530

    Added C++ solution to 'Finding Sasuke'

---
+++ Solutions/Finding Sasuke/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Finding Sasuke
+// Link to the problem: https://codeforces.com/contest/1413/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i += 2)
+    {
+        cout << -a[i + 1] << " " << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
