6fef6b397e35de6d182a2c2963607e59a83af074
Rushabh <20rush09abh2001@gmail.com>
2026-05-22 15:56:01 +0530

    Added C++ solution to 'Copy-paste'

---
+++ Solutions/Copy-paste/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Copy-paste
+// Link to the problem: https://codeforces.com/contest/1417/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll x = a[0];
+    ll ans = 0;
+    for (ll i = 1; i < n; i++)
+    {
+        ans += (k - a[i]) / x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
