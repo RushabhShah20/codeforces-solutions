42df36c2bb53bdce5d63e6375e5640074db3f0ca
Rushabh <20rush09abh2001@gmail.com>
2026-05-28 17:23:57 +0530

    Added C++ solution to 'Working Week'

---
+++ Solutions/Working Week/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Working Week
+// Link to the problem: https://codeforces.com/contest/1735/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n - 6) / 3;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
