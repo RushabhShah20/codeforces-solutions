6c835d015855e6047f85c7c77cfbb00ac9600a8a
Rushabh <20rush09abh2001@gmail.com>
2026-05-31 15:09:41 +0530

    Added C++ solution to 'Preparing for the Olympiad'

---
+++ Solutions/Preparing for the Olympiad/C++/main.cpp
@@ -0,0 +1,46 @@
+// Problem: Preparing for the Olympiad
+// Link to the problem: https://codeforces.com/contest/2051/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    ll ans = a[n - 1];
+    for (ll i = 0; i < n - 1; i++)
+    {
+        if (a[i] > b[i + 1])
+        {
+            ans += a[i] - b[i + 1];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
