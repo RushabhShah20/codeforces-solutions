89368d4e07bfca895791c3f8fef713b447dacc99
Rushabh <20rush09abh2001@gmail.com>
2026-05-31 15:35:37 +0530

    Added C++ solution to 'Turtle and Good Strings'

---
+++ Solutions/Turtle and Good Strings/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Turtle and Good Strings
+// Link to the problem: https://codeforces.com/contest/2003/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    const string ans = s[0] == s[n - 1] ? "NO" : "YES";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
