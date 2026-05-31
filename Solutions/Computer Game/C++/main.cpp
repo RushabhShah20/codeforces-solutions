a36cfb158443e36e17e2fe894c525cb9249cb6d9
Rushabh <20rush09abh2001@gmail.com>
2026-05-31 17:34:11 +0530

    Added C++ solution to 'Computer Game'

---
+++ Solutions/Computer Game/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Computer Game
+// Link to the problem: https://codeforces.com/contest/1598/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    for (ll i = 0; i < n; i++)
+    {
+        if (s[i] == '1' && t[i] == '1')
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
