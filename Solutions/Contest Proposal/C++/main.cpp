98667859419227e16adc2ad5449b03fe90d06980
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 14:58:09 +0530

    Added C++ solution to 'Contest Proposal'

---
+++ Solutions/Contest Proposal/C++/main.cpp
@@ -0,0 +1,50 @@
+// Problem: Contest Proposal
+// Link to the problem: https://codeforces.com/contest/1972/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    ll ans = 0, i = 0;
+    for (ll j = 0; j < n && i < n; j++)
+    {
+        if (a[i] > b[j])
+        {
+            ans++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
