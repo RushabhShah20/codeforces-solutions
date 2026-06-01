fdbccd7c5b65b254a3c3e2a00b8e74455db14508
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 18:02:16 +0530

    Added C++ solution to 'Make Equal'

---
+++ Solutions/Make Equal/C++/main.cpp
@@ -0,0 +1,47 @@
+// Problem: Make Equal
+// Link to the problem: https://codeforces.com/contest/1931/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        x += a[i];
+    }
+    x /= n;
+    ll y = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        y += a[i];
+        if (y < (i + 1) * x)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
