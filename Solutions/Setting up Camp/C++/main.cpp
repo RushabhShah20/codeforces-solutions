b75a72ab866f05b824d86ddab41c71c8b67e47bf
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 15:25:51 +0530

    Added C++ solution to 'Setting up Camp'

---
+++ Solutions/Setting up Camp/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Setting up Camp
+// Link to the problem: https://codeforces.com/contest/1945/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = (b % 3 == 1 && c < 2) || (b % 3 == 2 && c < 1) ? -1 : a + (b + c + 2) / 3;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
