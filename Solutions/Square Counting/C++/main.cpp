f814afe72a953dc0cd4e174d8d7a266125eef3d7
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 15:06:23 +0530

    Added C++ solution to 'Square Counting'

---
+++ Solutions/Square Counting/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Square Counting
+// Link to the problem: https://codeforces.com/contest/1646/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = k / (n * n);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
