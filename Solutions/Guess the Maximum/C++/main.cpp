7bac1fde9796e362a2769178051d81653dab753d
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 16:47:42 +0530

    Added C++ solution to 'Guess the Maximum'

---
+++ Solutions/Guess the Maximum/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Guess the Maximum
+// Link to the problem: https://codeforces.com/contest/1979/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll x = LLONG_MAX;
+    for (ll i = 1; i < n; i++)
+    {
+        x = min(x, max(a[i], a[i - 1]));
+    }
+    const ll ans = x - 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
