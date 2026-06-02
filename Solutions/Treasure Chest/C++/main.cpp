698cc12c20f595c8ed5d4a3c284f5c0dd0a5711b
Rushabh <20rush09abh2001@gmail.com>
2026-06-02 16:51:49 +0530

    Added C++ solution to 'Treasure Chest'

---
+++ Solutions/Treasure Chest/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Treasure Chest
+// Link to the problem: https://codeforces.com/contest/1895/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y, k;
+    cin >> x >> y >> k;
+    const ll ans = x > y ? x : y + max(0LL, y - x - k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
