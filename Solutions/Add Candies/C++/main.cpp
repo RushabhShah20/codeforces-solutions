2416c9c38b36053dd26a539969eca809738766ad
Rushabh <20rush09abh2001@gmail.com>
2026-06-03 17:28:46 +0530

    Added C++ solution to 'Add Candies'

---
+++ Solutions/Add Candies/C++/main.cpp
@@ -0,0 +1,34 @@
+// Problem: Add Candies
+// Link to the problem: https://codeforces.com/contest/1447/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << n << endl;
+    for (ll i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
