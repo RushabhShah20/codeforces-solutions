e6e5ade05d2c85f89b065e64952efb73587333b6
Rushabh <20rush09abh2001@gmail.com>
2026-06-03 16:53:08 +0530

    Added C++ solution to 'Marketing Scheme'

---
+++ Solutions/Marketing Scheme/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Marketing Scheme
+// Link to the problem: https://codeforces.com/contest/1437/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll l, r;
+    cin >> l >> r;
+    const string ans = r / 2 >= l ? "NO" : "YES";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
