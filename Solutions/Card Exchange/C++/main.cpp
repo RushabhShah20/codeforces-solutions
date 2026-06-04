e28974da0c85bae34c43418d869fe789c6e6e4f7
Rushabh <20rush09abh2001@gmail.com>
2026-06-04 17:42:58 +0530

    Added C++ solution to 'Card Exchange'

---
+++ Solutions/Card Exchange/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Card Exchange
+// Link to the problem: https://codeforces.com/contest/1966/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    ll mx = 0;
+    for (const pair<ll, ll> i : m)
+    {
+        mx = max(mx, i.second);
+    }
+    const ll ans = mx < k ? n : k - 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
