e690282eca0cc83d067275dce36fa5a7ccdcc956
Rushabh <20rush09abh2001@gmail.com>
2026-06-04 20:35:52 +0530

    Added C++ solution to 'Farmer John's Challenge'

---
+++ Solutions/Farmer John's Challenge/C++/main.cpp
@@ -0,0 +1,48 @@
+// Problem: Farmer John's Challenge
+// Link to the problem: https://codeforces.com/contest/1942/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (k == 1)
+    {
+        for (ll i = 1; i <= n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    else if (k == n)
+    {
+        for (ll i = 1; i <= n; i++)
+        {
+            cout << 1 << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
