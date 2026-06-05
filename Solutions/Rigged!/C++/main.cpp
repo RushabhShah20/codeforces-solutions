551c8df457083edfd509f8ea3a460ad7f78f47ef
Rushabh <20rush09abh2001@gmail.com>
2026-06-05 15:15:37 +0530

    Added C++ solution to 'Rigged!'

---
+++ Solutions/Rigged!/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Rigged!
+// Link to the problem: https://codeforces.com/contest/1879/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i] >> b[i];
+    }
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] >= a[0] && b[i] >= b[0])
+        {
+            cout << -1 << endl;
+            return;
+        }
+    }
+    cout << a[0] << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
