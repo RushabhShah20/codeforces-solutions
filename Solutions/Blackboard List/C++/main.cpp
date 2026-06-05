681335e02dbb02b9e6c68b72d22015bf39fb7c60
Rushabh <20rush09abh2001@gmail.com>
2026-06-05 16:11:36 +0530

    Added C++ solution to 'Blackboard List'

---
+++ Solutions/Blackboard List/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Blackboard List
+// Link to the problem: https://codeforces.com/contest/1838/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll ans = a[0] < 0 ? a[0] : a[n - 1];
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
