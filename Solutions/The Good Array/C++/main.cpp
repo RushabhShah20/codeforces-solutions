e11cb87f4b35fd9600314af320ae18baf5788f69
Rushabh <20rush09abh2001@gmail.com>
2026-06-05 18:42:06 +0530

    Added C++ solution to 'The Good Array'

---
+++ Solutions/The Good Array/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: The Good Array
+// Link to the problem: https://codeforces.com/contest/1839/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll x = 1 + (n + k - 2) / k, ans = n > 1 ? max(x, 2LL) : x;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
