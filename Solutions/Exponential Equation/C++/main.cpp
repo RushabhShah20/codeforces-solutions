264735c2f1cf3341972886bb09a83cc693e27e26
Rushabh <20rush09abh2001@gmail.com>
2026-06-06 21:16:24 +0530

    Added C++ solution to 'Exponential Equation'

---
+++ Solutions/Exponential Equation/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Exponential Equation
+// Link to the problem: https://codeforces.com/contest/1787/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n & 1)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << 1 << " " << n / 2 << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
