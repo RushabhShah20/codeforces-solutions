72b9041902dc82b7bec567e1920f604ca3ed5bd0
Rushabh <20rush09abh2001@gmail.com>
2026-06-06 15:48:18 +0530

    Added C++ solution to 'Indivisible'

---
+++ Solutions/Indivisible/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Indivisible
+// Link to the problem: https://codeforces.com/contest/1818/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return;
+    }
+    if (n & 1)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    for (ll i = 1; i <= n; i += 2)
+    {
+        cout << i + 1 << " " << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
