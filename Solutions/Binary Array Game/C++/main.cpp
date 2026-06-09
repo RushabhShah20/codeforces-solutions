e3c25834fa5e1288237464998f9aac02414c180c
Rushabh <20rush09abh2001@gmail.com>
2026-06-09 17:21:52 +0530

    Added C++ solution to 'Binary Array Game'

---
+++ Solutions/Binary Array Game/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Binary Array Game
+// Link to the problem: https://codeforces.com/contest/2183/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    const string ans = a[0] == 1 || a[n - 1] == 1 ? "Alice" : "Bob";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
