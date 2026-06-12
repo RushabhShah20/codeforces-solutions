bc68165db3977a0c9e094cd26b1e7621fe0b7fe2
Rushabh <20rush09abh2001@gmail.com>
2026-06-12 16:15:24 +0530

    Added C++ solution to 'Interview'

---
+++ Solutions/Interview/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Interview
+// Link to the problem: https://codeforces.com/contest/631/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll x = 0, y = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll z;
+        cin >> z;
+        x |= z;
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        ll z;
+        cin >> z;
+        y |= z;
+    }
+    const ll ans = x + y;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
