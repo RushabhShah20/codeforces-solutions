3133c9978fff9be6838158878eac462df6f02530
Rushabh <20rush09abh2001@gmail.com>
2026-06-13 02:05:13 +0530

    Added C++ solution to 'Games on the Train'

---
+++ Solutions/Games on the Train/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Games on the Train
+// Link to the problem: https://codeforces.com/contest/2236/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll mn = LLONG_MAX, mx = LLONG_MIN;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        mn = min(mn, x);
+        mx = max(mx, x);
+    }
+    const ll ans = mx - mn + 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
