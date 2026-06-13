2aa90f7be8415bbcbf63e1f35e40fa2b008a4fb2
Rushabh <20rush09abh2001@gmail.com>
2026-06-13 16:06:38 +0530

    Added C++ solution to 'Fun Permutation'

---
+++ Solutions/Fun Permutation/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Fun Permutation
+// Link to the problem: https://codeforces.com/contest/2137/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> ans(n);
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ans[i] = n + 1 - x;
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
