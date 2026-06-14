0e03650b9d1c0674883822aae45469b55a090a42
Rushabh <20rush09abh2001@gmail.com>
2026-06-14 17:31:01 +0530

    Added C++ solution to 'Network Configuration'

---
+++ Solutions/Network Configuration/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Network Configuration
+// Link to the problem: https://codeforces.com/contest/412/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    const ll ans = a[k - 1];
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
