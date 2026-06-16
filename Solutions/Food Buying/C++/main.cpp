d54391c037d9cd2d4aad07f8cb834ea796db7d5d
Rushabh <20rush09abh2001@gmail.com>
2026-06-16 18:12:45 +0530

    Added C++ solution to 'Food Buying'

---
+++ Solutions/Food Buying/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Food Buying
+// Link to the problem: https://codeforces.com/contest/1296/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = n;
+    while (n >= 10)
+    {
+        const ll x = n / 10, y = n % 10;
+        n = x + y;
+        ans += x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
