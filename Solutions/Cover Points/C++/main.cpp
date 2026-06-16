443e7349a4e90ed9e95fe46817af5b2a2809fbea
Rushabh <20rush09abh2001@gmail.com>
2026-06-16 18:34:06 +0530

    Added C++ solution to 'Cover Points'

---
+++ Solutions/Cover Points/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Cover Points
+// Link to the problem: https://codeforces.com/contest/1047/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x, y;
+        cin >> x >> y;
+        ans = max(ans, x + y);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
