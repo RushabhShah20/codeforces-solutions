459e2047ef19bfd215952ecc584b881da7596e5d
Rushabh <20rush09abh2001@gmail.com>
2026-06-18 15:24:04 +0530

    Added C++ solution to 'DIY Wooden Ladder'

---
+++ Solutions/DIY Wooden Ladder/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: DIY Wooden Ladder
+// Link to the problem: https://codeforces.com/contest/1197/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    const ll ans = min(n - 2, a[n - 2] - 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
