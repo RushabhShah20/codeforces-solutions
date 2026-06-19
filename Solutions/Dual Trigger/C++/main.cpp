cc1592293829a449ebfb31063415cdceeda342e9
Rushabh <20rush09abh2001@gmail.com>
2026-06-19 21:25:33 +0530

    Added C++ solution to 'Dual Trigger'

---
+++ Solutions/Dual Trigger/C++/main.cpp
@@ -0,0 +1,48 @@
+// Problem: Dual Trigger
+// Link to the problem: https://codeforces.com/contest/1951/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        x += s[i] - '0';
+    }
+    if (x == 2)
+    {
+        for (ll i = 1; i < n; i++)
+        {
+            if (s[i] == '1' && s[i - 1] == '1')
+            {
+                cout << "NO" << endl;
+                return;
+            }
+        }
+    }
+    const string ans = x & 1 ? "NO" : "YES";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
