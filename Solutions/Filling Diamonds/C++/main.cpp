81affea8468e916062726bedf683c4caf29cbf78
Rushabh <20rush09abh2001@gmail.com>
2026-06-22 12:22:27 +0530

    Added C++ solution to 'Filling Diamonds'

---
+++ Solutions/Filling Diamonds/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Filling Diamonds
+// Link to the problem: https://codeforces.com/contest/1339/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << n << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
