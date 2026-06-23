4f2bd9c530d33d147b006c87e78cbc78ce992c26
Rushabh <20rush09abh2001@gmail.com>
2026-06-23 17:57:34 +0530

    Added C++ solution to 'Exams'

---
+++ Solutions/Exams/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Exams
+// Link to the problem: https://codeforces.com/contest/194/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = max(0LL, 3 * n - k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
