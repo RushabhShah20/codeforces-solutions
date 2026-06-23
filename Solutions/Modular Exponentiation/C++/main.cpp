93533bfbca43b2bb8544ad5fdf766eade1b305cd
Rushabh <20rush09abh2001@gmail.com>
2026-06-23 18:54:32 +0530

    Added C++ solution to 'Modular Exponentiation'

---
+++ Solutions/Modular Exponentiation/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Modular Exponentiation
+// Link to the problem: https://codeforces.com/contest/913/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    const ll ans = n >= 64 ? m : m % (1LL << n);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
