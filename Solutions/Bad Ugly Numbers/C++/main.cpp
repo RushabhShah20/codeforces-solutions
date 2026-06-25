fd9fb7641c79ee07bb470e87bd55e53fa2dd2993
Rushabh <20rush09abh2001@gmail.com>
2026-06-25 17:34:23 +0530

    Added C++ solution to 'Bad Ugly Numbers'

---
+++ Solutions/Bad Ugly Numbers/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Bad Ugly Numbers
+// Link to the problem: https://codeforces.com/contest/1326/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    const string ans = "2" + string(n - 1, '3');
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
