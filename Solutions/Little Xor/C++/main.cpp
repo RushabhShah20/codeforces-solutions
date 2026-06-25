71384438af3efed9644aaf0485817fec4449d0e2
Rushabh <20rush09abh2001@gmail.com>
2026-06-25 11:53:34 +0530

    Added C++ solution to 'Little Xor'

---
+++ Solutions/Little Xor/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Little Xor
+// Link to the problem: https://codeforces.com/contest/252/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x = 0;
+        for (ll j = i; j < n; j++)
+        {
+            x ^= a[j];
+            ans = max(ans, x);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
