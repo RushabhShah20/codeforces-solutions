005fdae2c56f8325f695ac8fcc7f0dfa4e5083f6
Rushabh <20rush09abh2001@gmail.com>
2026-06-25 11:35:59 +0530

    Added C++ solution to 'Fancy Fence'

---
+++ Solutions/Fancy Fence/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Fancy Fence
+// Link to the problem: https://codeforces.com/contest/270/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const string ans = 360 % (180 - n) == 0 ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
