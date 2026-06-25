97740a95b6229c1a90ecb6eb468464b06edb9ea6
Rushabh <20rush09abh2001@gmail.com>
2026-06-25 11:45:55 +0530

    Added C++ solution to 'Yaroslav and Permutations'

---
+++ Solutions/Yaroslav and Permutations/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Yaroslav and Permutations
+// Link to the problem: https://codeforces.com/contest/296/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    ll mx = 0;
+    for (const pair<ll, ll> i : m)
+    {
+        mx = max(mx, i.second);
+    }
+    const string ans = mx <= (n + 1) / 2 ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
