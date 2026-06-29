69ed13d366f582b72d6cbb5c815a85d19ace8d95
Rushabh <20rush09abh2001@gmail.com>
2026-06-29 18:21:31 +0530

    Added C++ solution to 'Buttons'

---
+++ Solutions/Buttons - 2/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Buttons
+// Link to the problem: https://codeforces.com/contest/268/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll m = n * n, ans = n * (m - 1) / 6 + n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
