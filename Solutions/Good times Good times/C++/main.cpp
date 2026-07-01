8bc8513a22ae3418222563fd6c3e8951abe5cc18
Rushabh <20rush09abh2001@gmail.com>
2026-07-01 11:11:54 +0530

    Added C++ solution to 'Good times Good times'

---
+++ Solutions/Good times Good times/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Good times Good times
+// Link to the problem: https://codeforces.com/contest/2241/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const ll ans = powl(10, s.size()) + 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
