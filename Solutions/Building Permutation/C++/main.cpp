ca74e4fd58ceca80e69b3a5f1e67f7a3deadca12
Rushabh <20rush09abh2001@gmail.com>
2026-07-01 15:26:27 +0530

    Added C++ solution to 'Building Permutation'

---
+++ Solutions/Building Permutation/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Building Permutation
+// Link to the problem: https://codeforces.com/contest/285/problem/C
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ans += abs(i + 1 - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
