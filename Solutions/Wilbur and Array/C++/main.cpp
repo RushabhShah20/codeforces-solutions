688f5ec1a38e4b813fa311fca57f8ba4eb3ebbf2
Rushabh <20rush09abh2001@gmail.com>
2026-07-02 20:26:25 +0530

    Added C++ solution to 'Wilbur and Array'

---
+++ Solutions/Wilbur and Array/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Wilbur and Array
+// Link to the problem: https://codeforces.com/contest/596/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0, y = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ans += abs(y - x);
+        y = x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
