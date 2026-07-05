7d98aca4ef48f03757cdd952ac3afa3e56c32b18
Rushabh <20rush09abh2001@gmail.com>
2026-07-05 20:00:57 +0530

    Added C++ solution to 'Chess Tourney'

---
+++ Solutions/Chess Tourney/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Chess Tourney
+// Link to the problem: https://codeforces.com/contest/845/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(2 * n);
+    for (ll i = 0; i < 2 * n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    const string ans = a[n - 1] > a[n] ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
