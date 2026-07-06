29bfd3d1a5c5aa62aa57ca5943d1531c3f811db0
Rushabh <20rush09abh2001@gmail.com>
2026-07-06 18:34:36 +0530

    Added C++ solution to 'Polycarp Training'

---
+++ Solutions/Polycarp Training/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Polycarp Training
+// Link to the problem: https://codeforces.com/contest/1165/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = 0, x = 1;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] >= x)
+        {
+            ans++;
+            x++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
