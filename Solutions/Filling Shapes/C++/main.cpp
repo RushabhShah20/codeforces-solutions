d9b64cd84410892ac3f26bbd5e346fbcdf7aafbc
Rushabh <20rush09abh2001@gmail.com>
2026-07-06 19:38:33 +0530

    Added C++ solution to 'Filling Shapes'

---
+++ Solutions/Filling Shapes/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Filling Shapes
+// Link to the problem: https://codeforces.com/contest/1182/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n & 1 ? 0 : 1LL << (n / 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
