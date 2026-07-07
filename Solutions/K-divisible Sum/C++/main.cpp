082b23d0a2fc4dd7415267f6e2a29494436e3823
Rushabh <20rush09abh2001@gmail.com>
2026-07-07 20:23:21 +0530

    Added C++ solution to 'K-divisible Sum'

---
+++ Solutions/K-divisible Sum/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: K-divisible Sum
+// Link to the problem: https://codeforces.com/contest/1476/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = (k * ((n + k - 1) / k) + n - 1) / n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
