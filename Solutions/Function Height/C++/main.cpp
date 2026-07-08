1378bb39f15c8727c0f40f074f313b945ac9c879
Rushabh <20rush09abh2001@gmail.com>
2026-07-08 17:57:18 +0530

    Added C++ solution to 'Function Height'

---
+++ Solutions/Function Height/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Function Height
+// Link to the problem: https://codeforces.com/contest/1036/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = (k + n - 1) / n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
