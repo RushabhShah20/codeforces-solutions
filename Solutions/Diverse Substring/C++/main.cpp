631be8855a5c097493cdf549e141039c9ea67b4e
Rushabh <20rush09abh2001@gmail.com>
2026-07-08 19:51:12 +0530

    Added C++ solution to 'Diverse Substring'

---
+++ Solutions/Diverse Substring/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Diverse Substring
+// Link to the problem: https://codeforces.com/contest/1073/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (ll i = 1; i < n; i++)
+    {
+        if (s[i] != s[i - 1])
+        {
+            cout << "YES" << endl;
+            cout << s[i - 1] << s[i] << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
