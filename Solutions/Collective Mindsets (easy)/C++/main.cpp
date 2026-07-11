b05c6b2623e198b048753d656f968911ced87aa0
Rushabh <20rush09abh2001@gmail.com>
2026-07-11 20:18:19 +0530

    Added C++ solution to 'Collective Mindsets (easy)'

---
+++ Solutions/Collective Mindsets (easy)/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Collective Mindsets (easy)
+// Link to the problem: https://codeforces.com/contest/690/problem/A1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n + 1) >> 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
