2de75f46beae02dcb38ed64c155266adab60664a
Rushabh <20rush09abh2001@gmail.com>
2026-07-12 23:00:45 +0530

    Added C++ solution to 'Vasya's Calendar'

---
+++ Solutions/Vasya's Calendar/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Vasya's Calendar
+// Link to the problem: https://codeforces.com/contest/182/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll k, n;
+    cin >> k >> n;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        ans += i < n - 1 ? k - x : 0;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
