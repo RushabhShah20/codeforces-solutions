4d09d210a4a92ddb63bf1c47d2455f77f7810ede
Rushabh <20rush09abh2001@gmail.com>
2026-07-15 16:36:53 +0530

    Added C++ solution to 'Nikita and Books'

---
+++ Solutions/Nikita and Books/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Nikita and Books
+// Link to the problem: https://codeforces.com/contest/2244/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] + x < i + 1)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+        x += a[i] - (i + 1);
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
