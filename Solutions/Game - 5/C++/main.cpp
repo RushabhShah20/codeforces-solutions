c550c480cd9a8a0f398ec3c1c07d4b9ebf6babec
Rushabh <20rush09abh2001@gmail.com>
2026-07-17 14:58:07 +0530

    Added C++ solution to 'Game'

---
+++ Solutions/Game - 5/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Game
+// Link to the problem: https://codeforces.com/contest/630/problem/R
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = n & 1 ? 1 : 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
