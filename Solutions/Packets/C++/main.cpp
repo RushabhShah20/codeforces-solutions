80b4ede5fff290da3540b7d69ba835f267b06a71
Rushabh <20rush09abh2001@gmail.com>
2026-07-19 18:35:21 +0530

    Added C++ solution to 'Packets'

---
+++ Solutions/Packets/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Packets
+// Link to the problem: https://codeforces.com/contest/1037/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = __bit_width(n);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
