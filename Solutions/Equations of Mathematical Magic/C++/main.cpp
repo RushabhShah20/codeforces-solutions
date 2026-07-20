19a4c3236ca4cb377dfe480f7a96b95cad8b7ae5
Rushabh <20rush09abh2001@gmail.com>
2026-07-20 17:33:54 +0530

    Added C++ solution to 'Equations of Mathematical Magic'

---
+++ Solutions/Equations of Mathematical Magic/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Equations of Mathematical Magic
+// Link to the problem: https://codeforces.com/contest/1064/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = 1LL << __builtin_popcount(n);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
