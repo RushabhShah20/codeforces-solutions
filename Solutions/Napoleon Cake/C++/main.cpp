845d64fcc3e17ff5ab3b3fb815535555a4a52515
Rushabh <20rush09abh2001@gmail.com>
2026-08-01 23:19:30 +0530

    Added C++ solution to 'Napoleon Cake'

---
+++ Solutions/Napoleon Cake/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Napoleon Cake
+// Link to the problem: https://codeforces.com/contest/1501/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = n - 2; i >= 0; i--)
+    {
+        a[i] = max(a[i], max(0LL, a[i + 1] - 1));
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << (a[i] > 0 ? 1 : 0) << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
