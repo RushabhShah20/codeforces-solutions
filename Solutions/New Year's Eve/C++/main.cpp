c9dd53b727b67fc05476697221e79b825b16e787
Rushabh <20rush09abh2001@gmail.com>
2026-08-08 17:28:16 +0530

    Added C++ solution to 'New Year's Eve'

---
+++ Solutions/New Year's Eve/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: New Year's Eve
+// Link to the problem: https://codeforces.com/contest/912/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll m = __bit_width(n), ans = k == 1 ? n : stoll(string(m, '1'), nullptr, 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
