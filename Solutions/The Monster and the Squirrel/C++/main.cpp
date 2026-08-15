9d2c56fd873c9a3d3cb0f3e48691aebe189a9c9e
Rushabh <20rush09abh2001@gmail.com>
2026-08-15 21:52:02 +0530

    Added C++ solution to 'The Monster and the Squirrel'

---
+++ Solutions/The Monster and the Squirrel/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: The Monster and the Squirrel
+// Link to the problem: https://codeforces.com/contest/592/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n - 2) * (n - 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
