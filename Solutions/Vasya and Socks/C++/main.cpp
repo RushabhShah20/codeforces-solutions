34761557d730068cfe1de49b5029a4cb5337b441
Rushabh <20rush09abh2001@gmail.com>
2026-08-19 09:40:20 +0530

    Added C++ solution to 'Vasya and Socks'

---
+++ Solutions/Vasya and Socks/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Vasya and Socks
+// Link to the problem: https://codeforces.com/contest/460/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    const ll ans = n + (n - 1) / (m - 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
