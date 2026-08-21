423542c20b390a6dbf81a79912a2014827ac31e5
Rushabh <20rush09abh2001@gmail.com>
2026-08-21 19:41:06 +0530

    Added C++ solution to 'Odds and Ends'

---
+++ Solutions/Odds and Ends/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Odds and Ends
+// Link to the problem: https://codeforces.com/contest/849/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    const string ans = n & 1 && a[0] & 1 && a[n - 1] & 1 ? "Yes" : "No";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
