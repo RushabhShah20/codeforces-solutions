9e08cfcc684006323baad10a41941439e87a1dcd
Rushabh <20rush09abh2001@gmail.com>
2026-08-27 18:13:26 +0530

    Added C++ solution to 'Dreamoon and Ranking Collection'

---
+++ Solutions/Dreamoon and Ranking Collection/C++/main.cpp
@@ -0,0 +1,57 @@
+// Problem: Dreamoon and Ranking Collection
+// Link to the problem: https://codeforces.com/contest/1330/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    unordered_set<ll> s;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        s.insert(x);
+    }
+    ll ans = 1;
+    while (true)
+    {
+        if (s.count(ans) == 0)
+        {
+            if (k > 0)
+            {
+                k--;
+                ans++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            ans++;
+        }
+    }
+    ans--;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
