0a4e4ede872e577fdf75021323f438f319f49f08
Rushabh <20rush09abh2001@gmail.com>
2026-09-03 17:59:35 +0530

    Added C++ solution to 'Anti-Fibonacci Permutation'

---
+++ Solutions/Anti-Fibonacci Permutation/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Anti-Fibonacci Permutation
+// Link to the problem: https://codeforces.com/contest/1644/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+        for (ll j = n; j >= 1; j--)
+        {
+            if (i != j)
+            {
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
