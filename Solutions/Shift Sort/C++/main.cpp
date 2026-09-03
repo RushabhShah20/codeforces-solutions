54c2ccfce82eb90531c642ab04578d361645679c
Rushabh <20rush09abh2001@gmail.com>
2026-09-03 22:43:26 +0530

    Added C++ solution to 'Shift Sort'

---
+++ Solutions/Shift Sort/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Shift Sort
+// Link to the problem: https://codeforces.com/contest/2140/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string t = s;
+    sort(t.begin(), t.end());
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        x += s[i] == t[i] ? 0 : 1;
+    }
+    const ll ans = x / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
