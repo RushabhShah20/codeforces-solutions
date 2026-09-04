0d0a49e7aea46c9f3470334828b401edb53e41e5
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 17:33:37 +0530

    Added C++ solution to 'Sasha and the Drawing'

---
+++ Solutions/Sasha and the Drawing/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Sasha and the Drawing
+// Link to the problem: https://codeforces.com/contest/1929/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = k == 4 * n - 2 ? k / 2 + 1 : (k + 1) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
