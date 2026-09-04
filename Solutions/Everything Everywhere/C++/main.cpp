e1b0851c543cab406a51f2a33b6bdf31f0de745f
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 16:27:45 +0530

    Added C++ solution to 'Everything Everywhere'

---
+++ Solutions/Everything Everywhere/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Everything Everywhere
+// Link to the problem: https://codeforces.com/contest/2226/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (ll i = 1; i < n; i++)
+    {
+        ans += abs(a[i] - a[i - 1]) == __gcd(a[i], a[i - 1]) ? 1 : 0;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
