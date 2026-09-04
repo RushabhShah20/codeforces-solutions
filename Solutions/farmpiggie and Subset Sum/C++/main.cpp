d482028821c39c7de963519253e4d3d02636e14e
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 11:46:06 +0530

    Added C++ solution to 'farmpiggie and Subset Sum'

---
+++ Solutions/farmpiggie and Subset Sum/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: farmpiggie and Subset Sum
+// Link to the problem: https://codeforces.com/contest/2246/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 2; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    cout << 1 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
