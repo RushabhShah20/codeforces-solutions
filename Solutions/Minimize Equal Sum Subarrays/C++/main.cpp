760f30aa5e353413f0b327a1e0d2dc60e974334b
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 20:02:06 +0530

    Added C++ solution to 'Minimize Equal Sum Subarrays'

---
+++ Solutions/Minimize Equal Sum Subarrays/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Minimize Equal Sum Subarrays
+// Link to the problem: https://codeforces.com/contest/1998/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 1; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << a[0] << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
