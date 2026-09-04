24a6eef7ed632f653e0fcc58486fef882b082f24
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 17:40:24 +0530

    Added C++ solution to 'Z mod X = C'

---
+++ Solutions/Z mod X = C/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Z mod X = C
+// Link to the problem: https://codeforces.com/contest/1684/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll x = a + b + c, y = b + c, z = c;
+    cout << x << " " << y << " " << z << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
