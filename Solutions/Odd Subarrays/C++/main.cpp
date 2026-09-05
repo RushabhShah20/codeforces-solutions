f1bf003f4874e18e0cb80b461009d04f84590670
Rushabh <20rush09abh2001@gmail.com>
2026-09-05 15:01:12 +0530

    Added C++ solution to 'Odd Subarrays'

---
+++ Solutions/Odd Subarrays/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Odd Subarrays
+// Link to the problem: https://codeforces.com/contest/1686/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            ans++;
+            i++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
