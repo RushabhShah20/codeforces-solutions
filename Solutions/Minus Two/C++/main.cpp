2f4d5fec0983e026c9e03e37cc1807de604fb9df
Rushabh <20rush09abh2001@gmail.com>
2026-09-06 15:01:16 +0530

    Added C++ solution to 'Minus Two'

---
+++ Solutions/Minus Two/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Minus Two
+// Link to the problem: https://codeforces.com/contest/2259/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(4);
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        a[x % 4]++;
+    }
+    const ll ans = max({a[0], a[2], a[1] + a[3]});
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
