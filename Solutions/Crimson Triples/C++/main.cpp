0f13d3b5bf81a5b350d7cd4e27934c68b45b5aad
Rushabh <20rush09abh2001@gmail.com>
2026-09-07 20:16:55 +0530

    Added C++ solution to 'Crimson Triples'

---
+++ Solutions/Crimson Triples/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Crimson Triples
+// Link to the problem: https://codeforces.com/contest/2238/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        const ll x = n / i;
+        ans += x * x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
