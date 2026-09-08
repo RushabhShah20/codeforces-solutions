a6640859865553681c071a1aa9ad004dae1c3337
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 22:41:06 +0530

    Added C++ solution to 'Array'

---
+++ Solutions/Array - 2/C++/main.cpp
@@ -0,0 +1,47 @@
+// Problem: Array
+// Link to the problem: https://codeforces.com/contest/2209/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n), c(n), ans(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = i + 1; j < n; j++)
+        {
+            b[i] += a[i] > a[j] ? 1 : 0;
+            c[i] += a[i] < a[j] ? 1 : 0;
+        }
+        ans[i] = max(b[i], c[i]);
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
