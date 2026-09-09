dc3ab1b8739e273af329075c7032782d51dd850b
Rushabh <20rush09abh2001@gmail.com>
2026-09-09 16:26:20 +0530

    Added C++ solution to 'Yet Another Palindrome Problem'

---
+++ Solutions/Yet Another Palindrome Problem/C++/main.cpp
@@ -0,0 +1,45 @@
+// Problem: Yet Another Palindrome Problem
+// Link to the problem: https://codeforces.com/contest/1324/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = i + 2; j < n; j++)
+        {
+            if (a[i] == a[j])
+            {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
