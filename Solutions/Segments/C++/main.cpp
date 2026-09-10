e28458ce4a95511f3e5ba748c81e703bd3cb39b6
Rushabh <20rush09abh2001@gmail.com>
2026-09-10 20:24:52 +0530

    Added C++ solution to 'Segments'

---
+++ Solutions/Segments/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Segments
+// Link to the problem: https://codeforces.com/contest/909/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll m = (n / 2) + 1, ans = n & 1 ? m * m : m * (m - 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
