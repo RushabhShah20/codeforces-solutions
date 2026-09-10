f044c2ceda08486844464da5c71cc795e0b56370
Rushabh <20rush09abh2001@gmail.com>
2026-09-10 19:46:49 +0530

    Added C++ solution to 'Chloe and the sequence'

---
+++ Solutions/Chloe and the sequence/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Chloe and the sequence
+// Link to the problem: https://codeforces.com/contest/743/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll ans = 1;
+    while (!(k & 1))
+    {
+        ans++;
+        k >>= 1;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
