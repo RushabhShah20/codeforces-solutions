a6df1e892828a60f43d71bd8c5dd3ff2a96c9643
Rushabh <20rush09abh2001@gmail.com>
2026-09-11 15:32:15 +0530

    Added C++ solution to 'Painting the Ribbon'

---
+++ Solutions/Painting the Ribbon/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Painting the Ribbon
+// Link to the problem: https://codeforces.com/contest/1954/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    const string ans = n - (n + m - 1) / m > k ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
