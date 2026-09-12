3a0144275084595e11bae786f6eb6e7ccfbd42ef
Rushabh <20rush09abh2001@gmail.com>
2026-09-12 16:23:13 +0530

    Added C++ solution to 'Creating a Character'

---
+++ Solutions/Creating a Character/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Creating a Character
+// Link to the problem: https://codeforces.com/contest/1217/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = max(0LL, c - min(max(0LL, (c + b - a + 2) / 2), c + 1) + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
