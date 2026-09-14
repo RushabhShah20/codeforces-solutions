270820efde7810c0fa24f1b1299a90a073e4e55a
Rushabh <20rush09abh2001@gmail.com>
2026-09-14 16:38:54 +0530

    Added C++ solution to 'Nastya Is Playing Computer Games'

---
+++ Solutions/Nastya Is Playing Computer Games/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Nastya Is Playing Computer Games
+// Link to the problem: https://codeforces.com/contest/1136/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = 3 * n + min(k - 1, n - k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
