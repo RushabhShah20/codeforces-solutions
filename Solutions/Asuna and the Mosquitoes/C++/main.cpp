36035beaf92bca4a97c092cd88e4257ae7f92abe
Rushabh <20rush09abh2001@gmail.com>
2026-09-20 10:12:59 +0530

    Added C++ solution to 'Asuna and the Mosquitoes'

---
+++ Solutions/Asuna and the Mosquitoes/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Asuna and the Mosquitoes
+// Link to the problem: https://codeforces.com/contest/2092/problem/C
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a = 0, b = 0, c = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        a += x;
+        b += x & 1;
+        c = max(c, x);
+    }
+    const ll ans = b == 0 || b == n ? c : a - b + 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
