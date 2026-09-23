fade2d298b3d09e3f7571769590ff23899b080f2
Rushabh <20rush09abh2001@gmail.com>
2026-09-23 10:35:44 +0530

    Added C++ solution to 'Binary Cafe'

---
+++ Solutions/Binary Cafe/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Binary Cafe
+// Link to the problem: https://codeforces.com/contest/1840/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = min(n + 1, 1LL << min(k, 32LL));
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
