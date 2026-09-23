4f9ad318941dcc467e72db528bbe432f8c423825
Rushabh <20rush09abh2001@gmail.com>
2026-09-23 10:22:47 +0530

    Added C++ solution to 'AND Sorting'

---
+++ Solutions/AND Sorting/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: AND Sorting
+// Link to the problem: https://codeforces.com/contest/1682/problem/B
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = -1;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] != i)
+        {
+            ans = ans == -1 ? a[i] : ans & a[i];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
