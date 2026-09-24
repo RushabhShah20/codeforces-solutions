753f95634e8044b4c63bce21489493a96633ebf6
Rushabh <20rush09abh2001@gmail.com>
2026-09-24 17:57:31 +0530

    Added C++ solution to 'The Bento Box Adventure'

---
+++ Solutions/The Bento Box Adventure/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: The Bento Box Adventure
+// Link to the problem: https://codeforces.com/contest/2041/problem/A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll ans = 15;
+    for (ll i = 0; i < 4; i++)
+    {
+        ll x;
+        cin >> x;
+        ans -= x;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
